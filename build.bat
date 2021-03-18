4b4335f6eb7dbd0d448d2673a7c80c2c396e5f35
Yiwen-Chan <yiwen_chan@foxmail.com>
2021-03-18 19:27:27 +0800

    📦 更新使用方法

--- build.bat
+++ build.bat
@@ -1,3 +1,6 @@
-SET CGO_ENABLED=1
-go build -ldflags="-s -w  -extldflags '-static'" main.go
+go version
+gcc -v
+go env -w GOPROXY=https://goproxy.cn,direct
+go env -w GO111MODULE=auto
+go build -ldflags="-s -w" -o ZeroBot-Plugin-windows.exe
 pause
