66ff92e0645864a1fcb888e1d4da9b6562b15610
Yiwen-Chan <yiwen_chan@foxmail.com>
2021-04-16 19:18:29 +0800

    ⚡️ 更好的识图触发模式

--- setutime/utils/saucenao.go
+++ setutime/utils/saucenao.go
@@ -1,18 +1,17 @@
 package utils
 
 import (
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
-	"strings"
 
 	"github.com/tidwall/gjson"
+	"github.com/wdvxdr1123/ZeroBot/message"
 )
 
 // SauceNaoSearch SauceNao 以图搜图 需要链接 返回错误和信息
-func SauceNaoSearch(pic string) (text string, err error) {
+func SauceNaoSearch(pic string) (message.Message, error) {
 	var (
 		api    = "https://saucenao.com/search.php"
 		apiKey = "2cc2772ca550dbacb4c35731a79d341d1a143cb5"
@@ -20,65 +19,59 @@ func SauceNaoSearch(pic string) (text string, err error) {
 		minSimilarity = 70.0 // 返回图片结果的最小相似度
 	)
 
-	transport := http.Transport{
-		DisableKeepAlives: true,
-	}
-	client := &http.Client{
-		Transport: &transport,
-	}
-
 	// 包装请求参数
-	data := url.Values{}
-	data.Set("url", pic)         // 图片链接
-	data.Set("api_key", apiKey)  // api_key
-	data.Set("db", "5")          // 只搜索Pixiv
-	data.Set("numres", "1")      // 返回一个结果
-	data.Set("output_type", "2") // 返回JSON格式数据
-	fromData := strings.NewReader(data.Encode())
+	link, _ := url.Parse(api)
+	link.RawQuery = url.Values{
+		"url":         []string{pic},
+		"api_key":     []string{apiKey},
+		"db":          []string{"5"},
+		"numres":      []string{"1"},
+		"output_type": []string{"2"},
+	}.Encode()
 
 	// 网络请求
-	req, err := http.NewRequest("POST", api, fromData)
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", link.String(), nil)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	req.Header.Set("Accept", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0) Gecko/20100101 Firefox/6.0")
 	resp, err := client.Do(req)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
-	if code := resp.StatusCode; code != 200 {
+	if resp.StatusCode != http.StatusOK {
 		// 如果返回不是200则立刻抛出错误
-		return "", errors.New(fmt.Sprintf("SauceNAO not found, code %d", code))
+		return nil, fmt.Errorf("SauceNAO not found, code %d", resp.StatusCode)
 	}
 	content := gjson.ParseBytes(body)
 	if status := content.Get("header.status").Int(); status != 0 {
 		// 如果json信息返回status不为0则立刻抛出错误
-		return "", errors.New(fmt.Sprintf("SauceNAO not found, status %d", status))
+		return nil, fmt.Errorf("SauceNAO not found, status %d", status)
 	}
 	if content.Get("results.0.header.similarity").Float() < minSimilarity {
-		return "", errors.New("SauceNAO not found")
+		return nil, fmt.Errorf("SauceNAO not found")
 	}
+	result := content.Get("results.0")
 	// 正常发送
-	return fmt.Sprintf(
-		`[SetuTime] 我有把握是这个！[CQ:image,file=%s]相似度：%s%%
-标题：%s
-插画ID：%d
-画师：%s
-画师ID：%d
-直链：https://pixivel.moe/detail?id=%d`,
-		content.Get("results.0.header.thumbnail").Str,
-		content.Get("results.0.header.similarity").Str,
-		content.Get("results.0.data.title").Str,
-		content.Get("results.0.data.pixiv_id").Int(),
-		content.Get("results.0.data.member_name").Str,
-		content.Get("results.0.data.member_id").Int(),
-		content.Get("results.0.data.pixiv_id").Int(),
-	), nil
+	return message.Message{
+		message.Text("[SetuTime] 我有把握是这个！"),
+		message.Image(result.Get("header.thumbnail").Str),
+		message.Text(
+			"\n",
+			"相似度：", result.Get("header.similarity").Str, "\n",
+			"标题：", result.Get("data.title").Str, "\n",
+			"插画ID：", result.Get("data.pixiv_id").Int(), "\n",
+			"画师：", result.Get("data.member_name").Str, "\n",
+			"画师ID：", result.Get("data.member_id").Int(), "\n",
+			"直链：", "https://pixivel.moe/detail?id=", result.Get("data.pixiv_id").Int(),
+		),
+	}, nil
 }
