699ca04e0fe980ad4627be4c6fd903f91fa0db0c
Yiwen-Chan <yiwen_chan@foxmail.com>
2021-04-17 19:17:58 +0800

    ✨ 增加 shindanmaker.com 的新玩法

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 	_ "github.com/Yiwen-Chan/ZeroBot-Plugin/manager"
 	_ "github.com/Yiwen-Chan/ZeroBot-Plugin/music"
 	_ "github.com/Yiwen-Chan/ZeroBot-Plugin/runcode"
+	_ "github.com/Yiwen-Chan/ZeroBot-Plugin/shindan"
 	//_ "github.com/Yiwen-Chan/ZeroBot-Plugin/setutime"
 )
 
