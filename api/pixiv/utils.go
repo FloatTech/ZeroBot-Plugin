33dc16956d77c99dff1130e3107ec0250d6da71c
fumiama <s862105088@gmail.com>
2021-06-04 13:45:50 +0800

    分离搜图与随机图(随机图需要cgo而搜图不用)

--- api/pixiv/utils.go
+++ api/pixiv/utils.go
@@ -1,4 +1,4 @@
-package utils
+package pixiv
 
 import (
 	"os"
--- main.go
+++ main.go
@@ -23,10 +23,11 @@ import (
 	_ "github.com/Yiwen-Chan/ZeroBot-Plugin/runcode" // 在线运行代码
 
 	// 娱乐类
-	_ "github.com/Yiwen-Chan/ZeroBot-Plugin/music"    // 点歌
-	_ "github.com/Yiwen-Chan/ZeroBot-Plugin/randimg"  //简易随机图片
-	_ "github.com/Yiwen-Chan/ZeroBot-Plugin/setutime" // 涩图
-	_ "github.com/Yiwen-Chan/ZeroBot-Plugin/shindan"  // 测定
+	_ "github.com/Yiwen-Chan/ZeroBot-Plugin/music"       // 点歌
+	_ "github.com/Yiwen-Chan/ZeroBot-Plugin/picsearcher" // 搜图
+	_ "github.com/Yiwen-Chan/ZeroBot-Plugin/randimg"     //简易随机图片
+	_ "github.com/Yiwen-Chan/ZeroBot-Plugin/setutime"    // 涩图
+	_ "github.com/Yiwen-Chan/ZeroBot-Plugin/shindan"     // 测定
 )
 
 func init() {
