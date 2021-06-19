754057f3c268f2996c53e7e6b6080b1b2e2a9a47
fumiama <s862105088@gmail.com>
2021-06-19 14:37:13 +0800

    增加二次元图片评价功能

---
+++ api/context/pic.go
@@ -0,0 +1,58 @@
+package context
+
+import (
+	"strconv"
+	"time"
+
+	zero "github.com/wdvxdr1123/ZeroBot"
+)
+
+// HasPicture 消息含有图片返回 true
+func HasPicture() zero.Rule {
+	return func(ctx *zero.Ctx) bool {
+		msg := ctx.Event.Message
+		url := []string{}
+		// 如果是回复信息则将信息替换成被回复的那条
+		if msg[0].Type == "reply" {
+			id, _ := strconv.Atoi(msg[0].Data["id"])
+			msg = ctx.GetMessage(int64(id)).Elements
+		}
+		// 遍历信息中所有图片
+		for _, elem := range msg {
+			if elem.Type == "image" {
+				url = append(url, elem.Data["url"])
+			}
+		}
+		// 如果有图片就返回true
+		if len(url) > 0 {
+			ctx.State["image_url"] = url
+			return true
+		}
+		return false
+	}
+}
+
+// MustHasPicture 消息不存在图片阻塞60秒至有图片，超时返回 false
+func MustHasPicture() zero.Rule {
+	return func(ctx *zero.Ctx) bool {
+		if HasPicture()(ctx) {
+			return true
+		}
+		// 没有图片就索取
+		ctx.Send("请发送一张图片")
+		next := zero.NewFutureEvent("message", 999, false, zero.CheckUser(ctx.Event.UserID), HasPicture())
+		recv, cancel := next.Repeat()
+		select {
+		case e := <-recv:
+			cancel()
+			newCtx := &zero.Ctx{Event: e, State: zero.State{}}
+			if HasPicture()(newCtx) {
+				ctx.State["image_url"] = newCtx.State["image_url"]
+				return true
+			}
+			return false
+		case <-time.After(time.Second * 60):
+			return false
+		}
+	}
+}
--- randimg/setu_geter.go
+++ randimg/setu_geter.go
@@ -2,27 +2,21 @@ package randimg
 
 import (
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"net/url"
-	"os"
-	"strconv"
 	"strings"
 
+	"github.com/Yiwen-Chan/ZeroBot-Plugin/api/classify"
+	"github.com/Yiwen-Chan/ZeroBot-Plugin/api/context"
 	"github.com/Yiwen-Chan/ZeroBot-Plugin/api/msgext"
 	zero "github.com/wdvxdr1123/ZeroBot"
 )
 
 var (
-	RANDOM_API_URL          = "https://api.pixivweb.com/anime18r.php?return=img"
-	CLASSIFY_RANDOM_API_URL = "http://127.0.0.1:62002/dice?url=" + RANDOM_API_URL
-	VOTE_API_URL            = "http://saki.fumiama.top/vote?uuid=零号&img=%s&class=%d"
-	BLOCK_REQUEST_CLASS     = false
-	BLOCK_REQUEST           = false
-	CACHE_IMG_FILE          = "/tmp/setugt"
-	CACHE_URI               = "file:///" + CACHE_IMG_FILE
-	msgofgrp                = make(map[int64]int64)
-	dhashofmsg              = make(map[int64]string)
+	RANDOM_API_URL = "https://api.pixivweb.com/anime18r.php?return=img"
+
+	BLOCK_REQUEST = false
+
+	msgofgrp   = make(map[int64]int64)
+	dhashofmsg = make(map[int64]string)
 )
 
 func init() { // 插件主体
@@ -36,76 +30,16 @@ func init() { // 插件主体
 			}
 			return
 		})
-	zero.OnRegex(`^设置评价图片网址(.*)$`, zero.SuperUserPermission).SetBlock(true).SetPriority(20).
+	// 有保护的随机图片
+	zero.OnFullMatch("随机图片").SetBlock(true).SetPriority(24).
 		Handle(func(ctx *zero.Ctx) {
-			url := ctx.State["regex_matched"].([]string)[1]
-			if !strings.HasPrefix(url, "http") {
-				ctx.Send("URL非法!")
-			} else {
-				CLASSIFY_RANDOM_API_URL = url + RANDOM_API_URL
+			if ctx.Event.GroupID > 0 {
+				classify.Classify(ctx, RANDOM_API_URL, false)
 			}
 			return
 		})
-	// 有保护的随机图片
-	if CLASSIFY_RANDOM_API_URL != "" {
-		zero.OnFullMatch("评价图片").SetBlock(true).SetPriority(24).
-			Handle(func(ctx *zero.Ctx) {
-				if ctx.Event.GroupID > 0 {
-					if BLOCK_REQUEST_CLASS {
-						ctx.Send("请稍后再试哦")
-					} else {
-						BLOCK_REQUEST_CLASS = true
-						resp, err := http.Get(CLASSIFY_RANDOM_API_URL)
-						if err != nil {
-							ctx.Send(fmt.Sprintf("ERROR: %v", err))
-						} else {
-							class, err1 := strconv.Atoi(resp.Header.Get("Class"))
-							dhash := resp.Header.Get("DHash")
-							if err1 != nil {
-								ctx.Send(fmt.Sprintf("ERROR: %v", err1))
-							} else {
-								defer resp.Body.Close()
-								// 写入文件
-								data, _ := ioutil.ReadAll(resp.Body)
-								f, _ := os.OpenFile(CACHE_IMG_FILE, os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0644)
-								defer f.Close()
-								f.Write(data)
-								if class > 4 {
-									ctx.Send("太涩啦，不发了！")
-									if dhash != "" {
-										b14, err3 := url.QueryUnescape(dhash)
-										if err3 == nil {
-											ctx.Send("给你点提示哦：" + b14)
-										}
-									}
-								} else {
-									last_message_id := ctx.Send(msgext.ImageNoCache(CACHE_URI))
-									last_group_id := ctx.Event.GroupID
-									msgofgrp[last_group_id] = last_message_id
-									dhashofmsg[last_message_id] = dhash
-									if class > 2 {
-										ctx.Send("我好啦！")
-									}
-								}
-							}
-						}
-						BLOCK_REQUEST_CLASS = false
-					}
-				}
-				return
-			})
-	}
-
-	zero.OnFullMatch("不许好").SetBlock(true).SetPriority(24).
-		Handle(func(ctx *zero.Ctx) {
-			vote(ctx, 5)
-		})
-	zero.OnFullMatch("太涩了").SetBlock(true).SetPriority(24).
-		Handle(func(ctx *zero.Ctx) {
-			vote(ctx, 6)
-		})
-	// 	直接随机图片
-	zero.OnFullMatch("随机图片").SetBlock(true).SetPriority(24).
+	// 直接随机图片，危险，仅管理员可用
+	zero.OnFullMatch("直接随机", zero.AdminPermission).SetBlock(true).SetPriority(24).
 		Handle(func(ctx *zero.Ctx) {
 			if ctx.Event.GroupID > 0 {
 				if BLOCK_REQUEST {
@@ -120,17 +54,26 @@ func init() { // 插件主体
 			}
 			return
 		})
-}
-
-func vote(ctx *zero.Ctx, class int) {
-	msg, ok := msgofgrp[ctx.Event.GroupID]
-	if ok {
-		ctx.DeleteMessage(msg)
-		delete(msgofgrp, ctx.Event.GroupID)
-		dhash, ok2 := dhashofmsg[msg]
-		if ok2 {
-			http.Get(fmt.Sprintf(VOTE_API_URL, dhash, class))
-			delete(dhashofmsg, msg)
-		}
-	}
+	// 撤回最后的随机图片
+	zero.OnFullMatch("不许好").SetBlock(true).SetPriority(24).
+		Handle(func(ctx *zero.Ctx) {
+			classify.Vote(ctx, 5)
+		})
+	// 撤回最后的随机图片
+	zero.OnFullMatch("太涩了").SetBlock(true).SetPriority(24).
+		Handle(func(ctx *zero.Ctx) {
+			classify.Vote(ctx, 6)
+		})
+	// 上传一张图进行评价
+	zero.OnFullMatch("评价图片", context.MustHasPicture()).SetBlock(true).SetPriority(24).
+		Handle(func(ctx *zero.Ctx) {
+			if ctx.Event.GroupID > 0 {
+				ctx.Send("少女祈祷中......")
+				for _, pic := range ctx.State["image_url"].([]string) {
+					fmt.Println(pic)
+					classify.Classify(ctx, pic, true)
+				}
+			}
+			return
+		})
 }
