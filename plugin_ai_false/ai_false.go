de64756314d433c158ca433d7b2c7a739d28d116
Yiwen-Chan <yiwen_chan@foxmail.com>
2021-06-30 09:46:42 +0800

    🎨 修改文本格式

--- plugin_ai_false/ai_false.go
+++ plugin_ai_false/ai_false.go
@@ -1,6 +1,10 @@
-package plugin_i_false
+/*
+暂时只有服务器监控
+*/
+package plugin_ai_false
 
 import (
+	"math"
 	"time"
 
 	"github.com/shirou/gopsutil/cpu"
@@ -11,30 +15,30 @@ import (
 	"github.com/wdvxdr1123/ZeroBot/message"
 )
 
-func GetCpuPercent() float64 {
-	percent, _ := cpu.Percent(time.Second, false)
-	return percent[0]
-}
-
-func GetMemPercent() float64 {
-	memInfo, _ := mem.VirtualMemory()
-	return memInfo.UsedPercent
-}
-
-func GetDiskPercent() float64 {
-	parts, _ := disk.Partitions(true)
-	diskInfo, _ := disk.Usage(parts[0].Mountpoint)
-	return diskInfo.UsedPercent
-}
-
 func init() { // 插件主体
-	zero.OnFullMatchGroup([]string{"身体检查", "自检", "启动自检", "系统状态"}, zero.AdminPermission).
+	zero.OnFullMatchGroup([]string{"检查身体", "自检", "启动自检", "系统状态"}, zero.AdminPermission).
 		Handle(func(ctx *zero.Ctx) {
 			ctx.SendChain(message.Text(
-				"人家当前CPU占用率是:  ", GetCpuPercent(), "%\n",
-				"人家当前RAM占用率是:  ", GetMemPercent(), "%\n",
-				"人家当前硬盘活动率是:  ", GetDiskPercent(), "%\n",
+				"* CPU占用率: ", getCpuPercent(), "%\n",
+				"* RAM占用率: ", getMemPercent(), "%\n",
+				"* 硬盘活动率: ", getDiskPercent(), "%",
 			),
 			)
 		})
+}
+
+func getCpuPercent() float64 {
+	percent, _ := cpu.Percent(time.Second, false)
+	return math.Round(percent[0])
+}
+
+func getMemPercent() float64 {
+	memInfo, _ := mem.VirtualMemory()
+	return math.Round(memInfo.UsedPercent)
+}
+
+func getDiskPercent() float64 {
+	parts, _ := disk.Partitions(true)
+	diskInfo, _ := disk.Usage(parts[0].Mountpoint)
+	return math.Round(diskInfo.UsedPercent)
 }
