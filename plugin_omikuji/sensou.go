4dd7787bc429a6e47587476e4d3b4391e8ff69a1
fumiama <s862105088@gmail.com>
2021-08-25 22:00:11 +0800

    ✏️ 提高求签优先级

--- plugin_omikuji/sensou.go
+++ plugin_omikuji/sensou.go
@@ -13,7 +13,7 @@ const (
 )
 
 func init() { // 插件主体
-	zero.OnFullMatchGroup([]string{"求签", "运势", "占卜"}, zero.OnlyToMe).
+	zero.OnFullMatchGroup([]string{"求签", "运势", "占卜"}, zero.OnlyToMe).SetPriority(10).
 		Handle(func(ctx *zero.Ctx) {
 			miku := rand.Intn(100) + 1
 			ctx.SendChain(message.Image(fmt.Sprintf(bed, miku, 0)), message.Image(fmt.Sprintf(bed, miku, 1)))
