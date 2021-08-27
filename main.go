fb8ba1d66b216d55fc893e282aa0eb2325821fa9
散无友纪 <tdf261939@gmail.com>
2021-08-27 15:10:38 +0800

    Update main.go

--- main.go
+++ main.go
@@ -21,6 +21,7 @@ import (
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin_minecraft" // MCSManager
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin_music"     // 点歌
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin_shindan"   // 测定
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin_hs"        // 炉石
 
 	// b站相关
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin_bilibili" // 查询b站用户信息
