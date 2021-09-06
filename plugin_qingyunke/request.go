3ae333e0479e589e925afb18a6271160c210b1b6
Kanri <yiwen_chan@foxmail.com>
2021-09-06 14:58:04 +0800

    ✏️ 修复碧油鸡

--- plugin_qingyunke/request.go
+++ plugin_qingyunke/request.go
@@ -1,6 +1,5 @@
 package qingyunke
 
-
 import (
 	zero "github.com/wdvxdr1123/ZeroBot"
 )
@@ -9,11 +8,9 @@ import (
 func init() {
 	zero.OnRequest().SetBlock(false).FirstPriority().Handle(func(ctx *zero.Ctx) {
 		if ctx.Event.RequestType == "friend" {
-			log.Println("加好友")
 			ctx.SetFriendAddRequest(ctx.Event.Flag, true, "")
 		}
 		if ctx.Event.RequestType == "group" && ctx.Event.SubType == "invite" {
-			log.Println("加群")
 			ctx.SetGroupAddRequest(ctx.Event.Flag, "invite", true, "我爱你，mua~")
 		}
 	})
