ec7b4d17858127a2300e47840e64658c82fc3de6
fumiama <s862105088@gmail.com>
2021-09-10 14:00:46 +0800

    ✏️  修复首次启动可能 panic

--- control/register.go
+++ control/register.go
@@ -6,6 +6,6 @@ import (
 
 func Register(service string, o *Options) *zero.Engine {
 	engine := zero.New()
-	engine.UsePreHandler(New(service, o).Handler())
+	engine.UsePreHandler(new(service, o).Handler())
 	return engine
 }
