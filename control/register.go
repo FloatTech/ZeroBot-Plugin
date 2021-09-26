36f4d473f3d6f23c9d1aec44054dcc35b4c8f17c
源文雨 <41315874+fumiama@users.noreply.github.com>
2021-09-26 10:19:10 +0800

    Update register.go

--- control/register.go
+++ control/register.go
@@ -4,6 +4,7 @@ import (
 	zero "github.com/wdvxdr1123/ZeroBot"
 )
 
+// Register 注册插件控制器
 func Register(service string, o *Options) *zero.Engine {
 	engine := zero.New()
 	engine.UsePreHandler(new(service, o).Handler())
