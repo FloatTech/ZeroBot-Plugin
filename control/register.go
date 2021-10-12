92989ce9fd7fd2c624d53911ff4fc59004b04b3c
fumiama <s862105088@gmail.com>
2021-10-12 19:16:01 +0800

    ✨ control 增加 Delete

--- control/register.go
+++ control/register.go
@@ -10,3 +10,16 @@ func Register(service string, o *Options) *zero.Engine {
 	engine.UsePreHandler(newctrl(service, o).Handler())
 	return engine
 }
+
+// Delete 删除插件控制器，不会删除数据
+func Delete(engine *zero.Engine, service string) {
+	// engine.Delete()
+	mu.RLock()
+	_, ok := managers[service]
+	mu.RUnlock()
+	if ok {
+		mu.Lock()
+		delete(managers, service)
+		mu.Unlock()
+	}
+}
