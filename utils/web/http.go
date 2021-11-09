1304611ab71346cbd64d7018da5595c28770c37b
源文雨 <41315874+fumiama@users.noreply.github.com>
2021-11-09 12:41:08 +0800

    Update http.go

--- utils/web/http.go
+++ utils/web/http.go
@@ -7,11 +7,11 @@ import (
 
 func ReqWith(url string, method string, referer string, ua string) (data []byte, err error) {
 	client := &http.Client{}
-	//提交请求
+	// 提交请求
 	var reqest *http.Request
 	reqest, err = http.NewRequest(method, url, nil)
 	if err == nil {
-		//增加header选项
+		// 增加header选项
 		reqest.Header.Add("Referer", referer)
 		reqest.Header.Add("User-Agent", ua)
 		var response *http.Response
