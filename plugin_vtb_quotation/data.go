12f96bb95cdbe85148fdd12175b90798227f5257
fumiama <s862105088@gmail.com>
2021-11-22 22:33:29 +0800

    ✏️  修复 vtb panic 判断失误

--- plugin_vtb_quotation/data.go
+++ plugin_vtb_quotation/data.go
@@ -7,31 +7,36 @@ import (
 	"os"
 
 	"github.com/FloatTech/ZeroBot-Plugin/utils/file"
+	"github.com/FloatTech/ZeroBot-Plugin/utils/process"
 )
 
 const pburl = "https://codechina.csdn.net/u011570312/ZeroBot-Plugin/-/raw/master/" + dbfile
 
 // 加载数据库
 func init() {
-	_ = os.MkdirAll(dbpath, 0755)
-	if !file.IsExist(dbfile) { // 如果没有数据库，则从 url 下载
-		f, err := os.Create(dbfile)
-		if err != nil {
-			panic(err)
-		}
-		defer f.Close()
-		resp, err := http.Get(pburl)
-		if err == nil {
-			defer resp.Body.Close()
-			if resp.ContentLength > 0 {
-				log.Printf("[vtb]从镜像下载数据库%d字节...", resp.ContentLength)
-				data, err := io.ReadAll(resp.Body)
-				if err == nil && len(data) > 0 {
-					_, _ = f.Write(data)
-				}
+	go func() {
+		process.SleepAbout1sTo2s()
+		_ = os.MkdirAll(dbpath, 0755)
+		if !file.IsExist(dbfile) { // 如果没有数据库，则从 url 下载
+			f, err := os.Create(dbfile)
+			if err != nil {
 				panic(err)
 			}
+			defer f.Close()
+			resp, err := http.Get(pburl)
+			if err == nil {
+				defer resp.Body.Close()
+				if resp.ContentLength > 0 {
+					log.Printf("[vtb]从镜像下载数据库%d字节...", resp.ContentLength)
+					data, err := io.ReadAll(resp.Body)
+					if err == nil && len(data) > 0 {
+						_, _ = f.Write(data)
+						return
+					}
+					panic(err)
+				}
+			}
+			panic(err)
 		}
-		panic(err)
-	}
+	}()
 }
