fb475402257587bede5f1b0f70747c631c3da373
guohuiyuan <1156544355@qq.com>
2021-12-01 22:26:07 +0800

    fix:捕获协程异常

--- plugin_vtb_quotation/cron.go
+++ plugin_vtb_quotation/cron.go
@@ -10,6 +10,13 @@ import (
 
 func init() {
 	go func() {
+		defer func() {
+			//recover() //可以打印panic的错误信息
+			if err := recover(); err != nil { //产生了panic异常
+				log.Println(err)
+			}
+
+		}() //别忘了(), 调用此匿名函数
 		process.SleepAbout1sTo2s()
 		log.Println("[vtb/cron] 开启vtb数据库日常更新")
 		vtbDaily()
