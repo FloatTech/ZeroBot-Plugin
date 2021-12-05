95e5a0c6411022bd07b31238b7eea7d514978b82
himawari <1156544355@qq.com>
2021-12-05 22:48:40 +0800

    feat:添加书评功能

--- main.go
+++ main.go
@@ -28,17 +28,18 @@ import (
 
 	// 娱乐类
 	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin_wtf"    // 鬼东西
-	_ "github.com/FloatTech/ZeroBot-Plugin-Gif"              // 制图
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin_ai_false"  // 服务器监控
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin_choose"    // 选择困难症帮手
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin_fortune"   // 运势
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin_hs"        // 炉石
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin_minecraft" // MCSManager
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin_moyu"      // 摸鱼
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin_music"     // 点歌
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin_omikuji"   // 浅草寺求签
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin_reborn"    // 投胎
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin_shindan"   // 测定
+	_ "github.com/FloatTech/ZeroBot-Plugin-Gif"                // 制图
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin_ai_false"    // 服务器监控
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin_book_review" // 哀伤雪刃吧推书记录
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin_choose"      // 选择困难症帮手
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin_fortune"     // 运势
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin_hs"          // 炉石
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin_minecraft"   // MCSManager
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin_moyu"        // 摸鱼
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin_music"       // 点歌
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin_omikuji"     // 浅草寺求签
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin_reborn"      // 投胎
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin_shindan"     // 测定
 
 	// b站相关
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin_bilibili" // 查询b站用户信息
---
+++ plugin_book_review/data.go
@@ -0,0 +1,43 @@
+package plugin_book_review
+
+import (
+	"github.com/FloatTech/ZeroBot-Plugin/utils/file"
+	"github.com/FloatTech/ZeroBot-Plugin/utils/process"
+	log "github.com/sirupsen/logrus"
+	"io"
+	"net/http"
+	"os"
+)
+
+const dburl = "https://codechina.csdn.net/anto_july/bookreview/-/raw/master/bookreview.db"
+
+// 加载数据库
+func init() {
+	go func() {
+		process.SleepAbout1sTo2s()
+		// os.RemoveAll(dbpath)
+		_ = os.MkdirAll(dbpath, 0755)
+		if !file.IsExist(dbfile) { // 如果没有数据库，则从 url 下载
+			f, err := os.Create(dbfile)
+			if err != nil {
+				panic(err)
+			}
+			defer f.Close()
+			resp, err := http.Get(dburl)
+
+			if err == nil {
+				defer resp.Body.Close()
+				if resp.ContentLength > 0 {
+					log.Printf("[bookreview]从镜像下载数据库%d字节...", resp.ContentLength)
+					data, err := io.ReadAll(resp.Body)
+					if err == nil && len(data) > 0 {
+						_, _ = f.Write(data)
+						return
+					}
+					panic(err)
+				}
+			}
+			panic(err)
+		}
+	}()
+}
