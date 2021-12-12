5899723f3b7b844334a41651e291e04218ca89eb
himawari <1156544355@qq.com>
2021-12-12 22:08:34 +0800

    fix:去爬虫引入数据库

--- plugin_omikuji/data.go
+++ plugin_omikuji/data.go
@@ -1,13 +1,24 @@
 package omikuji
 
 import (
+	"github.com/FloatTech/ZeroBot-Plugin/utils/file"
+	"github.com/FloatTech/ZeroBot-Plugin/utils/sql"
+	"io"
+	"net/http"
 	"os"
 
 	log "github.com/sirupsen/logrus"
 
-	"github.com/FloatTech/ZeroBot-Plugin/plugin_omikuji/model"
 	"github.com/FloatTech/ZeroBot-Plugin/utils/process"
 )
+
+const (
+	dbpath = "data/omikuji/"
+	dbfile = dbpath + "signature.db"
+	dburl  = "https://codechina.csdn.net/anto_july/bookreview/-/raw/master/signature.db?inline=false"
+)
+
+var db = &sql.Sqlite{DBPath: dbfile}
 
 func init() {
 	go func() {
@@ -18,7 +29,37 @@ func init() {
 		}()
 		process.SleepAbout1sTo2s()
 		_ = os.MkdirAll(dbpath, 0755)
-		model.Initialize(dbfile)
+		if !file.IsExist(dbfile) { // 如果没有数据库，则从 url 下载
+			f, err := os.Create(dbfile)
+			if err != nil {
+				panic(err)
+			}
+			defer f.Close()
+			resp, err := http.Get(dburl)
+
+			if err != nil {
+				panic(err)
+			}
+			defer resp.Body.Close()
+			if resp.ContentLength > 0 {
+				log.Printf("[omikuji]从镜像下载数据库%d字节...", resp.ContentLength)
+				data, err := io.ReadAll(resp.Body)
+				if err == nil && len(data) > 0 {
+					_, _ = f.Write(data)
+				} else {
+					panic(err)
+				}
+			}
+		}
+		err := db.Create("signature", &signature{})
+		if err != nil {
+			panic(err)
+		}
+		n, err := db.Count("signature")
+		if err != nil {
+			panic(err)
+		}
+		log.Printf("[signature]读取%d条签文", n)
 	}()
 
 }
--- plugin_omikuji/sensou.go
+++ plugin_omikuji/sensou.go
@@ -4,30 +4,18 @@ package omikuji
 import (
 	"fmt"
 	"math/rand"
-	"regexp"
 	"strconv"
-	"strings"
 	"time"
 
 	"github.com/FloatTech/ZeroBot-Plugin/control"
-	"github.com/FloatTech/ZeroBot-Plugin/plugin_omikuji/model"
-	"github.com/FloatTech/ZeroBot-Plugin/utils/web"
-
 	log "github.com/sirupsen/logrus"
 
 	zero "github.com/wdvxdr1123/ZeroBot"
 	"github.com/wdvxdr1123/ZeroBot/message"
-	"github.com/wdvxdr1123/ZeroBot/utils/helper"
 )
 
 const (
-	bed          = "https://codechina.csdn.net/u011570312/senso-ji-omikuji/-/raw/main/%d_%d.jpg"
-	dbpath       = "data/omikuji/"
-	dbfile       = dbpath + "signature.db"
-	url          = "https://services.shen88.cn/chouqian/guanyinlingqian-${index}.html"
-	ua           = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36"
-	refer        = "https://services.shen88.cn/"
-	signatureReg = `(<p><strong>.*?)<p class="center">`
+	bed = "https://codechina.csdn.net/u011570312/senso-ji-omikuji/-/raw/main/%d_%d.jpg"
 )
 
 var (
@@ -39,58 +27,37 @@ var (
 )
 
 func init() { // 插件主体
-	rand.Seed(time.Now().UnixNano())
 
 	engine.OnFullMatchGroup([]string{"求签", "占卜"}).SetPriority(10).SetBlock(true).
 		Handle(func(ctx *zero.Ctx) {
-			miku := int64(rand.Intn(100) + 1)
 			userId := ctx.Event.UserID
-			db, err := model.Open(dbfile)
+			today, err := strconv.ParseInt(time.Now().Format("20060102"), 10, 64)
 			if err != nil {
-				log.Errorln(err)
-				return
+				log.Errorln("string转化为int64格式有问题:", err)
 			}
-			miku, err = db.GetSignature(userId, miku)
-			log.Println("miku为:", miku, "err为:", err)
+			seed := userId + today
+			rand.Seed(seed)
+			miku := rand.Intn(100) + 1
 			ctx.SendChain(
 				message.At(userId),
 				message.Image(fmt.Sprintf(bed, miku, 0)),
 				message.Image(fmt.Sprintf(bed, miku, 1)),
 			)
-			db.Close()
 		})
 	engine.OnFullMatchGroup([]string{"解签"}).SetPriority(10).SetBlock(true).
 		Handle(func(ctx *zero.Ctx) {
-			miku := int64(rand.Intn(100) + 1)
 			userId := ctx.Event.UserID
-			db, err := model.Open(dbfile)
+			today, err := strconv.ParseInt(time.Now().Format("20060102"), 10, 64)
 			if err != nil {
-				log.Errorln(err)
-				return
+				log.Errorln("string转化为int64格式有问题:", err)
 			}
-			miku, err = db.GetSignature(userId, miku)
-
-			reqURL := strings.Replace(url, "${index}", strconv.FormatInt(miku, 10), -1)
-			data, err := web.ReqWith(reqURL, "GET", refer, ua)
-			if err != nil {
-				log.Errorln("err为:", err)
-				return
-			}
-
-			res := helper.BytesToString(data)
-			reg := regexp.MustCompile(signatureReg)
-			text := reg.FindStringSubmatch(res)[1]
-
-			text = strings.Replace(text, "<p>", "", -1)
-			text = strings.Replace(text, "</p>", "\n", -1)
-			text = strings.Replace(text, "<strong>", "", -1)
-			text = strings.Replace(text, "</strong>", "", -1)
-			text = "\n" + text
+			seed := userId + today
+			rand.Seed(seed)
+			miku := rand.Intn(100) + 1
+			s := getSignatureById(miku)
 			ctx.SendChain(
 				message.At(userId),
-				message.Text(text),
+				message.Text(s.Text),
 			)
-			db.Close()
-
 		})
 }
