4c1926e395e0d4af0745175c9fa50b41197f0e75
fumiama <s862105088@gmail.com>
2021-12-14 13:41:24 +0800

    ✏️  fix fortune config

--- plugin_fortune/cfg.pb.go
+++ plugin_fortune/cfg.pb.go
@@ -1,5 +1,5 @@
 package fortune
 
 type Conf struct {
-	Kind map[int64]uint32 `protobuf:"bytes,1,rep" protobuf_key:"varint,1,opt" protobuf_val:"varint,2,opt"`
+	Kind map[int64]uint32 `protobuf:"bytes,1,rep" protobuf_key:"varint,1,opt" protobuf_val:"varint,0,opt"`
 }
