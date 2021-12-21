f4bc43e7a56a00b675eb2a5c9d7b68719af058fd
Kanri <yiwen_chan@foxmail.com>
2021-12-21 09:35:59 +0800

    ✏️ 修改 windows 日志样式

--- run.bat
+++ run.bat
@@ -3,5 +3,5 @@ go env -w GOPROXY=https://goproxy.cn,direct
 go env -w GO111MODULE=auto
 go mod tidy
 ::go build -ldflags="-s -w" -o ZeroBot-Plugin.exe
-go run main.go
+go run main.go main_win.go
 pause
