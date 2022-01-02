25f07afee7e95843726f33c790e972f606ae6163
himawari <1156544355@qq.com>
2022-01-02 17:07:33 +0800

    fix:清理缓存

--- plugin_mocking_bird/data.go
+++ plugin_mocking_bird/data.go
@@ -12,6 +12,7 @@ func init() {
 	go func() {
 		process.SleepAbout1sTo2s()
 		_ = os.MkdirAll(dbpath, 0755)
+		os.RemoveAll(cachePath)
 		_ = os.MkdirAll(cachePath, 0755)
 		_, err := file.GetLazyData(dbfile, false, true)
 		if err != nil {
