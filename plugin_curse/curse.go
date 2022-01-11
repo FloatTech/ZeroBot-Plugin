16e05e03201be9ebe810bed9704f2029f7799481
himawari <54976075+guohuiyuan@users.noreply.github.com>
2022-01-11 10:07:51 +0800

    独立骂人插件 (#102)
    * fix:vtb数据不用从zbp更新
    
    * feat:独立骂人插件
    
    * fix:修lint
    
    * feat:添加关键词
    
    Co-authored-by: Guohuiyuan <haibaraguo@yeahka.com>

---
+++ plugin_curse/curse.go
@@ -0,0 +1,50 @@
+// Package curse 骂人插件(求骂,自卫)
+package curse
+
+import (
+	"github.com/FloatTech/ZeroBot-Plugin/control"
+	"github.com/FloatTech/ZeroBot-Plugin/utils/process"
+	zero "github.com/wdvxdr1123/ZeroBot"
+	"github.com/wdvxdr1123/ZeroBot/extension/rate"
+	"github.com/wdvxdr1123/ZeroBot/message"
+	"time"
+)
+
+const (
+	prio     = 10
+	minLevel = "min"
+	maxLevel = "max"
+)
+
+var (
+	engine = control.Register("curse", &control.Options{
+		DisableOnDefault: false,
+		Help:             "骂人(求骂,自卫)\n- 骂我\n- 大力骂我\n- @bot 他妈|公交车|你妈|操|屎|去死|快死|日|逼|尼玛|艾滋|癌症|有病|戴套|啊对对对|烦你|你爹|屮|tui|cnm",
+	})
+	limit = rate.NewManager(time.Minute, 30)
+)
+
+func init() {
+	engine.OnFullMatch("骂我").SetBlock(true).SetPriority(prio).Handle(func(ctx *zero.Ctx) {
+		if !limit.Load(ctx.Event.GroupID).Acquire() {
+			return
+		}
+		process.SleepAbout1sTo2s()
+		text := getRandomCurseByLevel(minLevel).Text
+		ctx.SendChain(message.Reply(ctx.Event.MessageID), message.Text(text))
+	})
+
+	engine.OnFullMatch("大力骂我").SetBlock(true).SetPriority(prio).Handle(func(ctx *zero.Ctx) {
+		if !limit.Load(ctx.Event.GroupID).Acquire() {
+			return
+		}
+		process.SleepAbout1sTo2s()
+		text := getRandomCurseByLevel(maxLevel).Text
+		ctx.SendChain(message.Reply(ctx.Event.MessageID), message.Text(text))
+	})
+	engine.OnKeywordGroup([]string{"他妈", "公交车", "你妈", "操", "屎", "去死", "快死", "日", "逼", "尼玛", "艾滋", "癌症", "有病", "戴套", "啊对对对", "烦你", "你爹", "屮", "tui", "cnm"}, zero.OnlyToMe).SetBlock(true).SetPriority(prio).
+		Handle(func(ctx *zero.Ctx) {
+			text := getRandomCurseByLevel(maxLevel).Text
+			ctx.SendChain(message.Reply(ctx.Event.MessageID), message.Text(text))
+		})
+}
--- plugin_shadiao/shadiao.go
+++ plugin_shadiao/shadiao.go
@@ -10,28 +10,26 @@ import (
 )
 
 const (
-	chpURL     = "https://chp.shadiao.app/api.php"
-	duURL      = "https://du.shadiao.app/api.php"
-	pyqURL     = "https://pyq.shadiao.app/api.php"
-	yduanziURL = "http://www.yduanzi.com/duanzi/getduanzi"
-	chayiURL   = "https://api.lovelive.tools/api/SweetNothings/Web/0"
-	ganhaiURL  = "https://api.lovelive.tools/api/SweetNothings/Web/1"
-	// zuanURL         = "https://zuanbot.com/api.php?level=min&lang=zh_cn"
+	chpURL          = "https://chp.shadiao.app/api.php"
+	duURL           = "https://du.shadiao.app/api.php"
+	pyqURL          = "https://pyq.shadiao.app/api.php"
+	yduanziURL      = "http://www.yduanzi.com/duanzi/getduanzi"
+	chayiURL        = "https://api.lovelive.tools/api/SweetNothings/Web/0"
+	ganhaiURL       = "https://api.lovelive.tools/api/SweetNothings/Web/1"
 	ua              = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"
 	chpReferer      = "https://chp.shadiao.app/"
 	duReferer       = "https://du.shadiao.app/"
 	pyqReferer      = "https://pyq.shadiao.app/"
 	yduanziReferer  = "http://www.yduanzi.com/?utm_source=shadiao.app"
 	loveliveReferer = "https://lovelive.tools/"
-	// zuanReferer     = "https://zuanbot.com/"
-	prio = 10
+	prio            = 10
 )
 
 var (
 	engine = control.Register("shadiao", &control.Options{
 		DisableOnDefault: false,
 		Help: "沙雕app\n" +
-			"- 骂他[@xxx]|骂他[qq号](停用)\n- 骂我(停用)\n- 哄我\n- 渣我\n- 来碗绿茶\n- 发个朋友圈\n- 来碗毒鸡汤\n- 讲个段子",
+			"- 哄我\n- 渣我\n- 来碗绿茶\n- 发个朋友圈\n- 来碗毒鸡汤\n- 讲个段子",
 	})
 	limit = rate.NewManager(time.Minute, 60)
 )
