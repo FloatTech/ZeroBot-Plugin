d6037484a1e266c192e737c2b65467fd73b75ac7
himawari <1156544355@qq.com>
2022-01-15 02:40:37 +0800

    fix:更新旧状态

--- plugin_bilibili_push/bilibili_push.go
+++ plugin_bilibili_push/bilibili_push.go
@@ -26,15 +26,15 @@ import (
 )
 
 const (
-	ua              = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36"
-	referer         = "https://www.bilibili.com/"
-	infoURL         = "https://api.bilibili.com/x/space/acc/info?mid=%d"
-	userDynamicsURL = "https://api.vc.bilibili.com/dynamic_svr/v1/dynamic_svr/space_history?host_uid=%d&offset_dynamic_id=0&need_top=0"
-	liveListURL     = "https://api.live.bilibili.com/room/v1/Room/get_status_info_by_uids"
-	tURL            = "https://t.bilibili.com/"
-	liveURL         = "https://live.bilibili.com/"
-	prio            = 10
-	serviceName     = "bilibilipush"
+	ua             = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36"
+	referer        = "https://www.bilibili.com/"
+	infoURL        = "https://api.bilibili.com/x/space/acc/info?mid=%d"
+	userDynamicURL = "https://api.vc.bilibili.com/dynamic_svr/v1/dynamic_svr/space_history?host_uid=%d&offset_dynamic_id=0&need_top=0"
+	liveListURL    = "https://api.live.bilibili.com/room/v1/Room/get_status_info_by_uids"
+	tURL           = "https://t.bilibili.com/"
+	liveURL        = "https://live.bilibili.com/"
+	prio           = 10
+	serviceName    = "bilibilipush"
 )
 
 var (
@@ -281,7 +281,7 @@ func unsubscribeLive(buid, groupid int64) (err error) {
 }
 
 func getUserDynamicCard(buid int64) (cardList []gjson.Result) {
-	data, err := web.ReqWith(fmt.Sprintf(userDynamicsURL, buid), "GET", referer, ua)
+	data, err := web.ReqWith(fmt.Sprintf(userDynamicURL, buid), "GET", referer, ua)
 	if err != nil {
 		log.Errorln("[bilibilipush]:", err)
 	}
@@ -410,6 +410,8 @@ func sendLive() {
 					return true
 				})
 			}
+		} else {
+			liveStatus[key.Int()] = newStatus
 		}
 		return true
 	})
