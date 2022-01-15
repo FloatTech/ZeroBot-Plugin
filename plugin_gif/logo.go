48736d3f4acef889678efaf069c6521eb8e7c1d5
fumiama <s862105088@gmail.com>
2022-01-15 11:02:34 +0800

    💩👌 make lint happy

--- plugin_gif/logo.go
+++ plugin_gif/logo.go
@@ -7,13 +7,13 @@ import (
 	"github.com/FloatTech/zbputils/file"
 )
 
-func (c *context) prepareLogos(s ...string) {
+func (cc *context) prepareLogos(s ...string) {
 	for i, v := range s {
 		_, err := strconv.Atoi(v)
 		if err != nil {
-			_ = file.DownloadTo("https://gchat.qpic.cn/gchatpic_new//--"+strings.ToUpper(v)+"/0", c.usrdir+strconv.Itoa(i)+".gif", true)
+			_ = file.DownloadTo("https://gchat.qpic.cn/gchatpic_new//--"+strings.ToUpper(v)+"/0", cc.usrdir+strconv.Itoa(i)+".gif", true)
 		} else {
-			_ = file.DownloadTo("http://q4.qlogo.cn/g?b=qq&nk="+v+"&s=640", c.usrdir+strconv.Itoa(i)+".gif", true)
+			_ = file.DownloadTo("http://q4.qlogo.cn/g?b=qq&nk="+v+"&s=640", cc.usrdir+strconv.Itoa(i)+".gif", true)
 		}
 	}
 }
