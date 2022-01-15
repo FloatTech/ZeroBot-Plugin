6dbc0b389120d7918cc7a5e5ead4342a21b09ed0
fumiama <s862105088@gmail.com>
2022-01-15 20:25:07 +0800

    💩👌 make lint happy

--- order/prio.go
+++ order/prio.go
@@ -1,57 +1,105 @@
 // Package order 各个插件的优先级
 package order
 
-
 const (
+	//nolint: revive
 	PrioChat = iota * 10
+	//nolint: revive
 	PrioSleepManage
+	//nolint: revive
 	PrioAtri
+	//nolint: revive
 	PrioManager
 
+	//nolint: revive
 	PrioACGImage
+	//nolint: revive
 	PrioAIFalse
+	//nolint: revive
 	PrioAIWife
+	//nolint: revive
 	PrioBase14
+	//nolint: revive
 	PrioBilibili
+	//nolint: revive
 	PrioBilibiliPush
+	//nolint: revive
 	PrioBookReview
+	//nolint: revive
 	PrioCangTouShi
+	//nolint: revive
 	PrioChoose
+	//nolint: revive
 	PrioChouXiangHua
+	//nolint: revive
 	PrioCoser
+	//nolint: revive
 	PrioCPStory
+	//nolint: revive
 	PrioDanbooru
+	//nolint: revive
 	PrioDiana
+	//nolint: revive
 	PrioFansDaily
+	//nolint: revive
 	PrioFortune
+	//nolint: revive
 	PrioFunny
+	//nolint: revive
 	PrioGIF
+	//nolint: revive
 	PrioGithub
+	//nolint: revive
 	PrioHS
+	//nolint: revive
 	PrioImageFinder
+	//nolint: revive
 	PrioJueJueZi
+	//nolint: revive
 	PrioLolicon
+	//nolint: revive
 	PrioMinecraft
+	//nolint: revive
 	PrioMoyu
+	//nolint: revive
 	PrioMusic
+	//nolint: revive
 	PrioNativeSetu
+	//nolint: revive
 	PrioNativeWife
+	//nolint: revive
 	PrioNBNHHSH
+	//nolint: revive
 	PrioNovel
+	//nolint: revive
 	PrioOmikuji
+	//nolint: revive
 	PrioReborn
+	//nolint: revive
 	PrioRuncode
+	//nolint: revive
 	PrioSauceNao
+	//nolint: revive
 	PrioScore
+	//nolint: revive
 	PrioSetuTime
+	//nolint: revive
 	PrioShaDiao
+	//nolint: revive
 	PrioShinDan
+	//nolint: revive
 	PrioTraceMoe
+	//nolint: revive
 	PrioTranslation
+	//nolint: revive
 	PrioVtbQuotation
+	//nolint: revive
 	PrioWtf
 
+	//nolint: revive
 	PrioCurse
+	//nolint: revive
 	PrioMockingBird
+	//nolint: revive
 	PrioAIReply
 )
