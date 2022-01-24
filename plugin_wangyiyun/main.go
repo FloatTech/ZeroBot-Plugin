b376565a3fc87b5da82d9ed25162025a9f07b280
MoeMagicMango <i@himoyo.cn>
2022-01-24 20:26:33 +0800

    Add files via upload

--- plugin_wangyiyun/main.go
+++ plugin_wangyiyun/main.go
@@ -5,7 +5,6 @@ import
     
 	zero "github.com/wdvxdr1123/ZeroBot"
 	"github.com/wdvxdr1123/ZeroBot/message"
-	"github.com/wdvxdr1123/ZeroBot/utils/helper"
 
 	"github.com/FloatTech/zbputils/web"
 	
@@ -15,7 +14,14 @@ const(
     wangyiyunReferer = "http://ovooa.com/"
     	ua           = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"
     )
-func init() {
+    
+func init() { 
+    engine := control.Register("wangyiyun",, order.Priowangyiyun, &control.Options
+    {
+		DisableOnDefault: false,
+		help: "wangyiyun \n- 来份网易云热评",
+    })
+    
 	engine.OnFullMatch("来份网易云热评").SetBlock(true).Handle(func(ctx *zero.Ctx) {
 		if !limit.Load(ctx.Event.GroupID).Acquire() {
 			return
@@ -25,6 +31,6 @@ func init() {
 			ctx.SendChain(message.Text("ERROR:", err))
 			return
 		}
-		ctx.SendChain(message.Reply(ctx.Event.MessageID), message.Text(helper.BytesToString(data)))
+		
 	})
 }
