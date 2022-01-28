3e3b8f178ccfd9c249b37d93e3f2272e43840bc4
莫思敛 <55676105+shudorcl@users.noreply.github.com>
2022-01-28 20:37:23 +0800

    - 删去了遗漏的Trim

--- plugin_fortune/fortune.go
+++ plugin_fortune/fortune.go
@@ -11,7 +11,6 @@ import (
 	"math/rand"
 	"os"
 	"strconv"
-	"strings"
 	"time"
 
 	"github.com/fogleman/gg" // 注册了 jpg png gif
@@ -90,7 +89,7 @@ func init() {
 				// 个人用户设为负数
 				gid = -ctx.Event.UserID
 			}
-			i, ok := index[strings.Trim(ctx.State["regex_matched"].([]string)[1], " ")]
+			i, ok := index[ctx.State["regex_matched"].([]string)[1]]
 			if ok {
 				c, ok := control.Lookup("fortune")
 				if ok {
