cb0781bba613a4a3b8df993dd53190f9db1cbb55
Suika <290760339@qq.com>
2022-02-11 05:24:04 +0800

    Update video.go

--- plugin_asoul/video.go
+++ plugin_asoul/video.go
@@ -2,7 +2,7 @@ package asoul
 
 import (
 	"encoding/json"
-	"fmt"
+	log "github.com/sirupsen/logrus"
 	zero "github.com/wdvxdr1123/ZeroBot"
 	"github.com/wdvxdr1123/ZeroBot/message"
 	"math/rand"
@@ -12,21 +12,69 @@ import (
 )
 
 func init() {
-	engine.OnMessage().
+	engine.OnKeyword("来点然能量").
 		Handle(func(ctx *zero.Ctx) {
-			var data *vdInfo
-			switch ctx.Event.Message.String() {
-			case "来点然能量":
-				data = video(strconv.Itoa(diana))
-			case "来点晚能量":
-				data = video(strconv.Itoa(ava))
-			case "来点牛能量":
-				data = video(strconv.Itoa(kira))
-			case "来点乃能量":
-				data = video(strconv.Itoa(queen))
-			case "来点狼能量":
-				data = video(strconv.Itoa(carol))
-			}
+			data := video(strconv.Itoa(diana))
+			rand.Seed(time.Now().UnixNano())
+			ranNub := rand.Intn(50)
+			ctx.SendChain(message.Image(data.Data.List.Vlist[ranNub].Pic))
+			ctx.SendChain(message.CustomMusic(
+				"https://bilibili.com/video/"+data.Data.List.Vlist[ranNub].Bvid,
+				"11111112355",
+				data.Data.List.Vlist[ranNub].Title,
+			))
+		})
+}
+
+func init() {
+	engine.OnKeyword("来点晚能量").
+		Handle(func(ctx *zero.Ctx) {
+			data := video(strconv.Itoa(ava))
+			rand.Seed(time.Now().UnixNano())
+			ranNub := rand.Intn(50)
+			ctx.SendChain(message.Image(data.Data.List.Vlist[ranNub].Pic))
+			ctx.SendChain(message.CustomMusic(
+				"https://bilibili.com/video/"+data.Data.List.Vlist[ranNub].Bvid,
+				"11111112355",
+				data.Data.List.Vlist[ranNub].Title,
+			))
+		})
+}
+
+func init() {
+	engine.OnKeyword("来点牛能量").
+		Handle(func(ctx *zero.Ctx) {
+			data := video(strconv.Itoa(kira))
+			rand.Seed(time.Now().UnixNano())
+			ranNub := rand.Intn(50)
+			ctx.SendChain(message.Image(data.Data.List.Vlist[ranNub].Pic))
+			ctx.SendChain(message.CustomMusic(
+				"https://bilibili.com/video/"+data.Data.List.Vlist[ranNub].Bvid,
+				"11111112355",
+				data.Data.List.Vlist[ranNub].Title,
+			))
+		})
+}
+
+func init() {
+	engine.OnKeyword("来点乃能量").
+		Handle(func(ctx *zero.Ctx) {
+			data := video(strconv.Itoa(queen))
+			rand.Seed(time.Now().UnixNano())
+			ranNub := rand.Intn(50)
+			ctx.SendChain(message.Image(data.Data.List.Vlist[ranNub].Pic))
+			ctx.SendChain(message.CustomMusic(
+				"https://bilibili.com/video/"+data.Data.List.Vlist[ranNub].Bvid,
+				"11111112355",
+				data.Data.List.Vlist[ranNub].Title,
+			))
+		})
+}
+
+func init() {
+	engine.OnKeyword("来点狼能量").
+		Handle(func(ctx *zero.Ctx) {
+			data := video(strconv.Itoa(carol))
 			rand.Seed(time.Now().UnixNano())
 			ranNub := rand.Intn(50)
 			ctx.SendChain(message.Image(data.Data.List.Vlist[ranNub].Pic))
@@ -44,11 +92,11 @@ func video(uid string) *vdInfo {
 	client := &http.Client{}
 	req, err := http.NewRequest(method, url, nil)
 	if err != nil {
-		fmt.Println(err)
+		log.Errorf("[fans]", err)
 	}
 	res, err := client.Do(req)
 	if err != nil {
-		fmt.Println(err)
+		log.Errorf("[fans]", err)
 	}
 	defer res.Body.Close()
 	result := &vdInfo{}
