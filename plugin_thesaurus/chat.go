a233586dfe4d3ef00fa7c23ec52f27653e51fc70
MoeMagicMango <i@himoyo.cn>
2022-02-12 20:50:21 +0800

    Update chat.go

--- plugin_thesaurus/chat.go
+++ plugin_thesaurus/chat.go
@@ -18,7 +18,7 @@ const (
 
 var (
 	engine = control.Register("thesaurus", order.AcquirePrio(), &control.Options{
-		DisableOnDefault: ture,
+		DisableOnDefault: true,
 		Help:             "thesaurus\n- 稍微干净一点的chat回复 删掉了一些有点ex的w((",
 	})
 	kimogomap  = make(kimogo, 256)
