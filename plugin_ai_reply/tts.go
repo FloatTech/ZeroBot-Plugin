21d0fe33f3546d64520f0c9b289baaf5f2fe0c7b
Guohuiyuan <haibaraguo@yeahka.com>
2022-02-23 10:28:20 +0800

    fix:换一个正则函数

--- plugin_ai_reply/tts.go
+++ plugin_ai_reply/tts.go
@@ -24,11 +24,31 @@ const (
 )
 
 var (
-	reNumber   = "(\\-|\\+)?\\d+(\\.\\d+)?"
-	soundModes = [...]string{"拟声鸟阿梓", "拟声鸟药水哥", "百度女声", "百度男声", "百度度逍遥", "百度度丫丫"}
+	reNumber = "(\\-|\\+)?\\d+(\\.\\d+)?"
+	t        *ttsInstances
 )
 
+type ttsInstances struct {
+	m map[string]tts.TTS
+	l []string
+}
+
+func (t *ttsInstances) List() []string {
+	return t.l
+}
+
 func init() {
+	t = &ttsInstances{
+		m: map[string]tts.TTS{
+			"百度女声":   baidutts.NewBaiduTTS(0),
+			"百度男声":   baidutts.NewBaiduTTS(1),
+			"百度度逍遥":  baidutts.NewBaiduTTS(3),
+			"百度度丫丫":  baidutts.NewBaiduTTS(4),
+			"拟声鸟阿梓":  mockingbird.NewMockingBirdTTS(0),
+			"拟声鸟药水哥": mockingbird.NewMockingBirdTTS(1),
+		},
+		l: []string{"拟声鸟阿梓", "拟声鸟药水哥", "百度女声", "百度男声", "百度度逍遥", "百度度丫丫"},
+	}
 	engine := control.Register(ttsServiceName, order.AcquirePrio(), &control.Options{
 		DisableOnDefault: false,
 		Help:             "语音回复(包括拟声鸟和百度)\n- @Bot 任意文本(任意一句话回复)\n- 设置语音模式拟声鸟阿梓 | 设置语音模式拟声鸟药水哥 | 设置语音模式百度女声 | 设置语音模式百度男声| 设置语音模式百度度逍遥 | 设置语音模式百度度丫丫",
@@ -37,7 +57,7 @@ func init() {
 		Handle(func(ctx *zero.Ctx) {
 			msg := ctx.ExtractPlainText()
 			r := aireply.NewAIReply(getReplyMode(ctx))
-			tts := NewTTS(getSoundMode(ctx))
+			tts := newTTS(getSoundMode(ctx))
 			ctx.SendChain(message.Record(tts.Speak(ctx.Event.UserID, func() string {
 				reply := r.TalkPlain(msg, zero.BotConfig.NickName[0])
 				re := regexp.MustCompile(reNumber)
@@ -53,9 +73,9 @@ func init() {
 				return reply
 			})))
 		})
-	engine.OnPrefix(`设置语音模式`).SetBlock(true).
+	engine.OnRegex(`^设置语音模式(.*)$`, ctxext.FirstValueInList(t)).SetBlock(true).
 		Handle(func(ctx *zero.Ctx) {
-			param := ctx.State["args"].(string)
+			param := ctx.State["regex_matched"].([]string)[1]
 			err := setSoundMode(ctx, param)
 			if err != nil {
 				ctx.SendChain(message.Reply(ctx.Event.MessageID), message.Text(err))
@@ -65,24 +85,9 @@ func init() {
 		})
 }
 
-// NewTTS 语音简单工厂
-func NewTTS(name string) tts.TTS {
-	switch name {
-	case "百度女声":
-		return baidutts.NewBaiduTTS(0)
-	case "百度男声":
-		return baidutts.NewBaiduTTS(1)
-	case "百度度逍遥":
-		return baidutts.NewBaiduTTS(3)
-	case "百度度丫丫":
-		return baidutts.NewBaiduTTS(4)
-	case "拟声鸟阿梓":
-		return mockingbird.NewMockingBirdTTS(0)
-	case "拟声鸟药水哥":
-		return mockingbird.NewMockingBirdTTS(1)
-	default:
-		return mockingbird.NewMockingBirdTTS(0)
-	}
+// newTTS 语音简单工厂
+func newTTS(name string) tts.TTS {
+	return t.m[name]
 }
 
 func setSoundMode(ctx *zero.Ctx, name string) error {
@@ -92,15 +97,12 @@ func setSoundMode(ctx *zero.Ctx, name string) error {
 	}
 	var ok bool
 	var index int64
-	for i, s := range soundModes {
+	for i, s := range t.l {
 		if s == name {
 			ok = true
 			index = int64(i)
 			break
 		}
-	}
-	if !ok {
-		return errors.New("no such mode")
 	}
 	m, ok := control.Lookup(ttsServiceName)
 	if !ok {
@@ -117,8 +119,8 @@ func getSoundMode(ctx *zero.Ctx) (name string) {
 	m, ok := control.Lookup(ttsServiceName)
 	if ok {
 		index := m.GetData(gid)
-		if int(index) < len(soundModes) {
-			return soundModes[index]
+		if int(index) < len(t.l) {
+			return t.l[index]
 		}
 	}
 	return "拟声鸟阿梓"
