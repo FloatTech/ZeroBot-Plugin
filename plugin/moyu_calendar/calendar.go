772da52c200298d48d23159542f71b17b893a19c
林沐馨 <70338354+LingMuXing@users.noreply.github.com>
2022-03-07 23:05:39 +0800

    Update calendar.go

--- plugin/moyu_calendar/calendar.go
+++ plugin/moyu_calendar/calendar.go
@@ -66,39 +66,3 @@ func Base64Encoding(str string) string { //Base64编码
 	ress := base64.StdEncoding.EncodeToString(src) //将编码变成字符串
 	return ress
 }
-
-func cpuPercent() float64 {
-	percent, err := cpu.Percent(time.Second, false)
-	if err != nil {
-		return -1
-	}
-	return math.Round(percent[0])
-}
-
-func memPercent() float64 {
-	memInfo, err := mem.VirtualMemory()
-	if err != nil {
-		return -1
-	}
-	return math.Round(memInfo.UsedPercent)
-}
-
-func diskPercent() string {
-	parts, err := disk.Partitions(true)
-	if err != nil {
-		return err.Error()
-	}
-	msg := ""
-	for _, p := range parts {
-		diskInfo, err := disk.Usage(p.Mountpoint)
-		if err != nil {
-			msg += "\n  - " + err.Error()
-			continue
-		}
-		pc := uint(math.Round(diskInfo.UsedPercent))
-		if pc > 0 {
-			msg += fmt.Sprintf("\n  - %s(%dM) %d%%", p.Mountpoint, diskInfo.Total/1024/1024, pc)
-		}
-	}
-	return msg
-}
