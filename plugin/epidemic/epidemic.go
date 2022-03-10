91e6e60abf9b3f53bc7cdfcd339e89244ad4e1f6
Raniy <1923021@qq.com>
2022-03-10 21:30:16 +0800

    增加查询城市疫情

--- plugin/epidemic/epidemic.go
+++ plugin/epidemic/epidemic.go
@@ -2,26 +2,58 @@
 package epidemic
 
 import (
-	"fmt"
+	"encoding/json"
+	"strconv"
+	"strings"
 
 	control "github.com/FloatTech/zbputils/control"
 	"github.com/FloatTech/zbputils/control/order"
 	"github.com/FloatTech/zbputils/process"
 	"github.com/FloatTech/zbputils/web"
 	log "github.com/sirupsen/logrus"
-	"github.com/tidwall/gjson"
 	zero "github.com/wdvxdr1123/ZeroBot"
 	"github.com/wdvxdr1123/ZeroBot/message"
 )
 
 const (
-	servicename = "queryEpidemic"
+	servicename = "epidemic"
 	txurl       = "https://view.inews.qq.com/g2/getOnsInfo?name=disease_h5"
 )
 
-var (
-	isconfirm = false
-)
+type Result struct {
+	Ret  int    `json:"ret"`
+	Data string `json:"data"`
+}
+
+type Epidemic struct {
+	LastUpdateTime string `json:"lastUpdateTime"`
+	AreaTree       []Area `json:"areaTree"`
+}
+
+type Area struct {
+	Name  string `json:"name"`
+	Today struct {
+		Confirm int `json:"confirm"`
+	} `json:"today"`
+	Total struct {
+		NowConfirm int    `json:"nowConfirm"`
+		Confirm    int    `json:"confirm"`
+		Dead       int    `json:"dead"`
+		Heal       int    `json:"heal"`
+		Grade      string `json:"grade"`
+	} `json:"total"`
+	Children []Area `json:"children"`
+}
+
+type CityInfo struct {
+	Name           string
+	Confirm        int
+	NowConfirm     int
+	Dead           int
+	Heal           int
+	Grade          string
+	LastUpdateTime string
+}
 
 func init() {
 	engine := control.Register(servicename, order.AcquirePrio(), &control.Options{
@@ -31,69 +63,77 @@ func init() {
 	})
 	engine.OnSuffix("疫情").SetBlock(true).
 		Handle(func(ctx *zero.Ctx) {
-			region := ctx.State["regex_matched"].([]string)[1]
+			text := ""
+			for _, elem := range ctx.Event.Message {
+				if elem.Type == "text" {
+					text = strings.ReplaceAll(elem.Data["text"], " ", "")
+					text = text[:strings.LastIndex(text, "疫情")]
+					break
+				}
+			}
 			process.SleepAbout1sTo2s()
-			data, returnbool := queryEpidemic(region)
-			if returnbool {
-				ctx.SendChain(message.Text("没有找到【" + region + "】城市的疫情数据."))
+			data := queryEpidemic(text)
+			if data == nil {
+				ctx.SendChain(message.Text("没有找到【" + text + "】城市的疫情数据."))
 				return
 			}
-			msgtext := "新增：" + data["confirm"] +
-				" ,现有确诊：" + data["nowConfirm"] +
-				" ,治愈：" + data["heal"] +
-				" ,死亡：" + data["dead"] + " " + data["grade"]
+			msgtext := "【" + data.Name + "】疫情数据:\n" +
+				"新增：" + strconv.Itoa(data.Confirm) +
+				" ,现有确诊：" + strconv.Itoa(data.NowConfirm) +
+				" ,治愈：" + strconv.Itoa(data.Heal) +
+				" ,死亡：" + strconv.Itoa(data.Dead) + " " + data.Grade
 			ctx.SendChain(
-				message.Text("【"+data["name"]+"】疫情信息如下：\n"),
 				message.Text(msgtext),
 				message.Text("\n"),
-				message.Text("更新时间："+data["lastUpdateTime"]),
+				message.Text("更新时间："+data.LastUpdateTime),
 				message.Text("\n"),
 				message.Text("温馨提示：请大家做好防疫工作，出门带好口罩！"),
 			)
 		})
 }
 
-func queryEpidemic(findName string) (map[string]string, bool) {
-	returnbool := false
+// Rcity 查找城市
+func Rcity(a *Area, cityName string) *Area {
+	if a == nil {
+		return nil
+	}
+	if a.Name == cityName {
+		return a
+	}
+	for _, v := range a.Children {
+		if v.Name == cityName {
+			return &v
+		}
+		c := Rcity(&v, cityName)
+		if c != nil {
+			return c
+		}
+	}
+	return nil
+}
+
+// queryEpidemic 查询城市疫情
+func queryEpidemic(findCityName string) *CityInfo {
 	response, err := web.GetData(txurl)
 	if err != nil {
 		log.Errorln("[txurl-err]:", err)
 	}
-	info := gjson.ParseBytes(response)
-	data := gjson.Parse(info.Get("data").String())
-	dqnmae := data.Get("areaTree.0.children").Array()
-	dataInfo := make(map[string]string)
-outfor:
-	for _, v := range dqnmae {
-		if findName == fmt.Sprint(v.Get("name")) {
-			dataInfo["name"] = v.Get("name").String()
-			dataInfo["confirm"] = fmt.Sprint(v.Get("today.confirm").Int())
-			dataInfo["nowConfirm"] = fmt.Sprint(v.Get("total.nowConfirm").Int())
-			dataInfo["heal"] = fmt.Sprint(v.Get("total.heal").Int())
-			dataInfo["dead"] = fmt.Sprint(v.Get("total.dead").Int())
-			dataInfo["grade"] = ""
-			dataInfo["lastUpdateTime"] = data.Get("lastUpdateTime").String()
-			isconfirm = true
-			break outfor
-		} else {
-			for _, cv := range v.Get("children").Array() {
-				if findName == fmt.Sprint(cv.Get("name")) {
-					dataInfo["name"] = v.Get("name").String() + "-" + cv.Get("name").String()
-					dataInfo["confirm"] = fmt.Sprint(cv.Get("today.confirm").Int())
-					dataInfo["nowConfirm"] = fmt.Sprint(cv.Get("total.nowConfirm").Int())
-					dataInfo["heal"] = fmt.Sprint(cv.Get("total.heal").Int())
-					dataInfo["dead"] = fmt.Sprint(cv.Get("total.dead").Int())
-					dataInfo["grade"] = cv.Get("total.grade").String()
-					dataInfo["lastUpdateTime"] = data.Get("lastUpdateTime").String()
-					isconfirm = true
-					break outfor
-				}
-			}
+	var r Result
+	_ = json.Unmarshal(response, &r)
+	var e Epidemic
+	_ = json.Unmarshal([]byte(r.Data), &e)
+	citydata := Rcity(&e.AreaTree[0], findCityName)
+	if citydata == nil {
+		return nil
+	} else {
+		return &CityInfo{
+			Name:           citydata.Name,
+			Confirm:        citydata.Today.Confirm,
+			NowConfirm:     citydata.Total.NowConfirm,
+			Dead:           citydata.Total.Dead,
+			Heal:           citydata.Total.Heal,
+			Grade:          citydata.Total.Grade,
+			LastUpdateTime: e.LastUpdateTime,
 		}
 	}
-	if !isconfirm {
-		returnbool = true
-	}
-	isconfirm = false
-	return dataInfo, returnbool
 }
