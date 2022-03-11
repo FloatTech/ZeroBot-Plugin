ebca124c8691cda799a151e0bba0f3a7ff547b37
源文雨 <41315874+fumiama@users.noreply.github.com>
2022-03-11 23:02:08 +0800

    🐛 修复 run

--- main.go
+++ main.go
@@ -77,6 +77,7 @@ import (
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/github"         // 搜索GitHub仓库
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/hs"             // 炉石
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/image_finder"   // 关键字搜图
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/inject"         // 注入指令
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/jandan"         // 煎蛋网无聊图
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/job"            // 定时指令触发器
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/juejuezi"       // 绝绝子生成器
