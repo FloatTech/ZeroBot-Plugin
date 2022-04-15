a62bd2e21aa173e75e84de1ebd96ef15c20f0e24
Jiang-Red <79574799+Jiang-Red@users.noreply.github.com>
2022-04-16 02:35:10 +0800

    Update main.go

--- main.go
+++ main.go
@@ -146,6 +146,7 @@ import (
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/groupwife" // 群老婆
 
 	// todo
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/dice"
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/personalrule"
 	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/tts"
 
