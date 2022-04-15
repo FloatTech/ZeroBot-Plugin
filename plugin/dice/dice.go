d9cf76cba0250fdd38e8b4666742f02e34b469d9
Jiang-Red <79574799+Jiang-Red@users.noreply.github.com>
2022-04-16 03:40:49 +0800

    Update dice.go

--- plugin/dice/dice.go
+++ plugin/dice/dice.go
@@ -22,11 +22,11 @@ func init() {
 			math, _ := strconv.Atoi(ctx.State["regex_matched"].([]string)[2])
 			r := rand.Intn(100) + 1
 			switch {
-			case r < math && r/2 < math/2 && r/5 < math/5:
+			case r < math && r < math/2 && r < math/5:
 				win := "极难成功"
 				msg := fmt.Sprintf("%s进行%s检定:\nD100=%d/%d %s", nickname, temp, r, math, win)
 				ctx.Send(msg)
-			case r < math && r/2 < math/2:
+			case r < math && r < math/2:
 				win := "困难成功"
 				msg := fmt.Sprintf("%s进行%s检定:\nD100=%d/%d %s", nickname, temp, r, math, win)
 				ctx.Send(msg)
