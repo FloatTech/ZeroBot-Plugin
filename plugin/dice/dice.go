bc01d4811eecb7540067614b8af0f61b86834b8b
Jiang-Red <79574799+Jiang-Red@users.noreply.github.com>
2022-04-16 03:56:28 +0800

    Update dice.go

--- plugin/dice/dice.go
+++ plugin/dice/dice.go
@@ -40,4 +40,31 @@ func init() {
 				ctx.Send(msg)
 			}
 		})
+	engine.OnRegex(`^.ra(\d+)(\D+)(\d+)`, zero.OnlyGroup).SetBlock(false).
+		Handle(func(ctx *zero.Ctx) {
+			nickname := ctx.CardOrNickName(ctx.Event.UserID)
+			i, _ := strconv.Atoi(ctx.State["regex_matched"].([]string)[1])
+			temp := ctx.State["regex_matched"].([]string)[2]
+			math, _ := strconv.Atoi(ctx.State["regex_matched"].([]string)[3])
+			msg := fmt.Sprintf("%s进行%s检定:\nD100=", nickname, temp)
+			for i > 0 && i < 30 {
+				i--
+				r := rand.Intn(100) + 1
+				switch {
+				case r < math && r < math/2 && r < math/5:
+					win := "极难成功"
+					msg += fmt.Sprintf("\nD100=%d/%d %s", r, math, win)
+				case r < math && r < math/2:
+					win := "困难成功"
+					msg += fmt.Sprintf("\nD100=%d/%d %s", r, math, win)
+				case r < math:
+					win := "成功"
+					msg += fmt.Sprintf("\nD100=%d/%d %s", r, math, win)
+				default:
+					win := "失败"
+					msg += fmt.Sprintf("\nD100=%d/%d %s", r, math, win)
+				}
+				ctx.Send(msg)
+			}
+		})
 }
