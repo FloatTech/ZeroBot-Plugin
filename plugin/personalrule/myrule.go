57dcd8493a173fd4560675e331c6a935c7999693
Jiang-Red <79574799+Jiang-Red@users.noreply.github.com>
2022-04-23 18:44:39 +0800

    Update myrule.go

--- plugin/personalrule/myrule.go
+++ plugin/personalrule/myrule.go
@@ -1,7 +1,7 @@
 package personalrule
 
 import (
-	"os/exec"
+	"fmt"
 
 	control "github.com/FloatTech/zbputils/control"
 
@@ -13,44 +13,31 @@ func init() {
 	engine := control.Register("mine", &control.Options{
 		DisableOnDefault: false,
 		Help: "Poweroff\n" +
-			"- kill/[Maybe]reboot: [@bot#kill|@bot#关机|@bot#poweroff|@bot#killbot]",
+			"- pause",
 	})
-	engine.OnFullMatchGroup([]string{"#kill", "#关机", "#poweroff", "#killbot"}, zero.OwnerPermission, zero.OnlyToMe).SetBlock(true).
+	engine.OnFullMatchGroup([]string{"pause", "restart", "kill"}, zero.OnlyToMe, zero.SuperUserPermission).SetBlock(false).
 		Handle(func(ctx *zero.Ctx) {
-			ctx.Send(message.Poke(ctx.Event.UserID))
-			err := exec.Command("rm", "-f", "/home/root/qqbot/session.token").Run()
-			ctx.Send(message.Text(err))
-			println(err)
-			err = exec.Command("killall", "-9", "go-cqhttp").Run()
-			ctx.Send(message.Text(err))
-			println(err)
-			err = exec.Command("killall", "-9", "zbp").Run()
-			ctx.Send(message.Text(err))
-			println(err)
-			err = exec.Command("rm", "-rf", "/home/root/qqbot/data/{leveldb-v2,Reborn,SetuTime,videos,images,VtbQuotation,sleep,hs,voices,cache,acgimage,Funny}").Run()
-			ctx.Send(message.Text(err))
-			println(err)
-			ctx.Send(message.Poke(ctx.Event.UserID))
-		},
-		)
-	engine.OnFullMatchGroup([]string{"#update", "#upgrade", "#updata", "#更新"}, zero.OwnerPermission, zero.OnlyToMe).SetBlock(true).
+			err := ""
+			panic(err)
+		})
+	engine.OnRegex(`^\[CQ:xml`, zero.OnlyGroup, zero.KeywordRule("serviceID=\"60\"")).SetBlock(true).
 		Handle(func(ctx *zero.Ctx) {
-			ctx.Send(message.Poke(ctx.Event.UserID))
-			err := exec.Command("rm", "-rf", "/home/root/qqbot/data/{leveldb-v2,Reborn,SetuTime,videos,images,VtbQuotation,sleep,hs,voices,cache,acgimage,Funny}").Run()
-			ctx.Send(message.Text(err))
-			println(err)
-			err = exec.Command("sh", "/home/root/qqbot/update.sh").Run()
-			ctx.Send(message.Text(err))
-			println(err)
-			err = exec.Command("sh", "/home/root/qqbot/qqbot.sh").Run()
-			ctx.Send(message.Text(err))
-			println(err)
-			ctx.Send(message.Poke(ctx.Event.UserID))
-		},
-		)
-	engine.OnKeywordGroup([]string{"戳我", "揉", "揉揉", "戳戳", "~", "poke"}, zero.OnlyToMe).SetBlock(true).
+			ctx.SetGroupKick(
+				ctx.Event.GroupID,
+				ctx.Event.UserID,
+				false,
+			)
+			nickname := ctx.GetGroupMemberInfo(
+				ctx.Event.GroupID,
+				ctx.Event.UserID,
+				false,
+			).Get("nickname").Str
+			ctx.SetGroupBan(ctx.Event.GroupID, ctx.Event.UserID, 7*24*60*60)
+			ctx.SendChain(message.ReplyWithMessage(ctx.Event.MessageID, message.Text(fmt.Sprintf("检测到 [%s](%d) 发送了干扰性消息,已处理", nickname, ctx.Event.UserID)))...)
+			ctx.DeleteMessage(ctx.Event.MessageID.(message.MessageID))
+		})
+	engine.OnRegex(`^来(.*)涩图`, zero.OnlyGroup, zero.KeywordRule("114514")).SetBlock(true).
 		Handle(func(ctx *zero.Ctx) {
-			ctx.Send(message.Poke(ctx.Event.UserID))
-		},
-		)
+			ctx.SendChain(message.Image("https://gchat.qpic.cn/gchatpic_new/1770747317/1049468946-3068097579-76A49478EFA68B4750B10B96917F7B58/0?term=3"))
+		})
 }
