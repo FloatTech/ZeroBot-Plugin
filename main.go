0794d4551c4feef079a8c06e04d4308890b11acd
Jiang-Red <79574799+Jiang-Red@users.noreply.github.com>
2022-05-05 23:50:58 +0800

    Update main.go

--- main.go
+++ main.go
@@ -116,7 +116,7 @@ import (
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/zaobao"         // 早报
 
 	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/wtf"            // 鬼东西
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/bilibili_push" // b站推送
+	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/bilibili_push" // b站推送
 
 	//                               ^^^^                               //
 	//                          ^^^^^^^^^^^^^^                          //
