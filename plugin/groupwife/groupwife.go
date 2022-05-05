4ea030d3332abf69364317610bb97d41ef2b7c76
Jiang-Red <79574799+Jiang-Red@users.noreply.github.com>
2022-05-05 23:50:41 +0800

    Update groupwife.go

--- plugin/groupwife/groupwife.go
+++ plugin/groupwife/groupwife.go
@@ -20,10 +20,14 @@ import (
 )
 
 var (
-	me      gjson.Result
-	list    gjson.Result
-	temp    []gjson.Result
-	listgid int64
+	me       gjson.Result
+	uid      int64
+	wifeid   int64
+	luid     int64
+	lwid     int64
+	sign     int64
+	wifename string
+	swife    = make(map[int64]int64)
 )
 
 func init() {
@@ -34,73 +38,65 @@ func init() {
 	})
 	engine.OnFullMatchGroup([]string{"哪个群友是我老婆", "哪位群友是我老婆", "今天谁是我老婆"}, zero.OnlyGroup).SetBlock(true).Limit(ctxext.LimitByGroup).
 		Handle(func(ctx *zero.Ctx) {
-			if listgid == 0 {
-				listgid = ctx.Event.GroupID
-			}
-			list = ctx.CallAction("get_group_member_list", zero.Params{
-				"group_id": listgid,
-				"no_cache": false,
-			}).Data
-			if len(temp) == 0 {
-				temp = list.Array()
-				sort.SliceStable(temp, func(i, j int) bool {
-					return temp[i].Get("last_sent_time").Int() < temp[j].Get("last_sent_time").Int()
-				})
-				temp = temp[math.Max(0, len(temp)-30):]
-			}
+			gid := ctx.Event.GroupID
 			uid := ctx.Event.UserID
+			for sign = range swife {
+				if sign == uid+gid {
+					uid = luid - gid
+					wifeid = swife[luid] - gid
+					wifename = ctx.CardOrNickName(wifeid)
+					avtar := fmt.Sprintf("[CQ:image,file=http://q4.qlogo.cn/g?b=qq&nk=%s&s=640,cache=0]", strconv.FormatInt(wifeid, 10))
+					msg := fmt.Sprintf("[CQ:at,qq=%d]今天你的群友老婆是%s\n【%s】(%d)哒！", uid, avtar, wifename, wifeid)
+					msg = message.UnescapeCQCodeText(msg)
+					ctx.SendGroupMessage(ctx.Event.GroupID, message.ParseMessageFromString(msg))
+					return
+				}
+			}
+			if swife[luid] == gid+ctx.Event.UserID {
+				uid = ctx.Event.UserID
+				wifeid = luid - gid
+				wifename = ctx.CardOrNickName(wifeid)
+				avtar := fmt.Sprintf("[CQ:image,file=http://q4.qlogo.cn/g?b=qq&nk=%s&s=640,cache=0]", strconv.FormatInt(wifeid, 10))
+				msg := fmt.Sprintf("[CQ:at,qq=%d]今天你的群友老婆是%s\n【%s】(%d)哒！", uid, avtar, wifename, wifeid)
+				msg = message.UnescapeCQCodeText(msg)
+				ctx.SendGroupMessage(ctx.Event.GroupID, message.ParseMessageFromString(msg))
+				return
+			}
+			list := ctx.CallAction("get_group_member_list", zero.Params{
+				"group_id": gid,
+				"no_cache": true,
+			}).Data
+			temp := list.Array()
+			sort.SliceStable(temp, func(i, j int) bool {
+				return temp[i].Get("last_sent_time").Int() < temp[j].Get("last_sent_time").Int()
+			})
+			temp = temp[math.Max(0, len(temp)-30):]
 			now := time.Now()
 			s := md5.Sum(helper.StringToBytes(fmt.Sprintf("%d%d%d%d", uid, now.Year(), now.Month(), now.Day())))
 			r := rand.New(rand.NewSource(int64(binary.LittleEndian.Uint64(s[:]))))
 			rn := r.Intn(len(temp))
 			who := temp[rn]
-			gid := ctx.Event.GroupID
-			if listgid != gid {
-				listgid = gid
-				list = ctx.CallAction("get_group_member_list", zero.Params{
-					"group_id": listgid,
-					"no_cache": false,
-				}).Data
-				temp = list.Array()
-				sort.SliceStable(temp, func(i, j int) bool {
-					return temp[i].Get("last_sent_time").Int() < temp[j].Get("last_sent_time").Int()
-				})
-				temp = temp[math.Max(0, len(temp)-30):]
-				rn = r.Intn(len(temp))
-				who = temp[rn]
-				listgid = gid
-			}
-			userid := who.Get("user_id").Int()
-			if userid == uid {
+			wifeid = who.Get("user_id").Int()
+			if wifeid == uid {
 				me = who
 				temp = append(temp[:rn], temp[rn:]...)
 				rn = r.Intn(len(temp))
 				who = temp[rn]
 			}
-			nick := who.Get("card").Str
-			if nick == "" {
-				nick = who.Get("nickname").Str
+			wifename = who.Get("card").Str
+			if wifename == "" {
+				wifename = who.Get("nickname").Str
 			}
 			avtar := fmt.Sprintf("[CQ:image,file=http://q4.qlogo.cn/g?b=qq&nk=%s&s=640,cache=0]", strconv.FormatInt(who.Get("user_id").Int(), 10))
-			msg := fmt.Sprintf("[CQ:at,qq=%d]今天你的群友老婆是%s\n【%s】(%d)哒！", uid, avtar, nick, userid)
+			msg := fmt.Sprintf("[CQ:at,qq=%d]今天你的群友老婆是%s\n【%s】(%d)哒！", uid, avtar, wifename, wifeid)
 			msg = message.UnescapeCQCodeText(msg)
 			ctx.SendGroupMessage(ctx.Event.GroupID, message.ParseMessageFromString(msg))
+			luid = gid + uid
+			lwid = gid + wifeid
+			swife[luid] = (lwid)
 			if len(me.Array()) != 0 {
 				mlist := append(temp[:rn], me)
 				temp = append(mlist, temp[rn:]...)
 			}
-		})
-	engine.OnFullMatch("换个老婆", zero.OnlyGroup, zero.SuperUserPermission).SetBlock(true).
-		Handle(func(ctx *zero.Ctx) {
-			list = ctx.CallAction("get_group_member_list", zero.Params{
-				"group_id": ctx.Event.GroupID,
-				"no_cache": true,
-			}).Data
-			temp = list.Array()
-			sort.SliceStable(temp, func(i, j int) bool {
-				return temp[i].Get("last_sent_time").Int() < temp[j].Get("last_sent_time").Int()
-			})
-			temp = temp[math.Max(0, len(temp)-30):]
-			ctx.SendChain(message.Text("换好了！"))
 		})
 }
