69210217f7e4e9b766402296742ff50950601dd1
Jiang-Red <79574799+Jiang-Red@users.noreply.github.com>
2022-05-08 18:02:37 +0800

    用上先进的新api

--- plugin/groupwife/groupwife.go
+++ plugin/groupwife/groupwife.go
@@ -2,19 +2,14 @@
 package groupwife
 
 import (
-	"crypto/md5"
-	"encoding/binary"
-	"fmt"
-	"math/rand"
 	"sort"
-	"time"
+	"strconv"
 
 	"github.com/FloatTech/zbputils/control"
 	"github.com/FloatTech/zbputils/ctxext"
 	"github.com/FloatTech/zbputils/math"
 	zero "github.com/wdvxdr1123/ZeroBot"
 	"github.com/wdvxdr1123/ZeroBot/message"
-	"github.com/wdvxdr1123/ZeroBot/utils/helper"
 )
 
 func init() {
@@ -35,18 +30,15 @@ func init() {
 			})
 			temp = temp[math.Max(0, len(temp)-30):]
 			uid := ctx.Event.UserID
-			now := time.Now()
-			s := md5.Sum(helper.StringToBytes(fmt.Sprintf("%d%d%d%d", uid, now.Year(), now.Month(), now.Day())))
-			r := rand.New(rand.NewSource(int64(binary.LittleEndian.Uint64(s[:]))))
-			who := temp[r.Intn(len(temp))]
+			who := temp[ctxext.RandSenderPerDayN(ctx, len(temp))]
 			wifename := who.Get("card").Str
 			wifeid := who.Get("user_id").Int()
 			if wifename == "" {
 				wifename = who.Get("nickname").Str
 			}
-			avtar := fmt.Sprintf("[CQ:image,file=http://q4.qlogo.cn/g?b=qq&nk=%d&s=640,cache=0]", wifeid)
-			msg := fmt.Sprintf("[CQ:at,qq=%d]今天你的群友老婆是%s\n【%s】(%d)哒！", uid, avtar, wifename, wifeid)
-			msg = message.UnescapeCQCodeText(msg)
-			ctx.SendGroupMessage(ctx.Event.GroupID, message.ParseMessageFromString(msg))
+			ctx.SendChain(message.At(uid),
+				message.Text("今天你的群友老婆是"),
+				message.Image("http://q4.qlogo.cn/g?b=qq&nk="+strconv.FormatInt(wifeid, 10)+"&s=640").Add("ceche", 0),
+				message.Text("\n【"+wifename+"】("+strconv.FormatInt(wifeid, 10)+")哒！"))
 		})
 }
