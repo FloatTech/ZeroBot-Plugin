c9ca2d1301d4f88311bfb55d6738b2a762c009da
1343396474@qq.com <1343396474@qq.com>
2022-05-12 00:05:37 +0800

    fix antirecall 没整明白，只好用一个蠢逻辑

--- plugin/antirecall/antirecall.go
+++ plugin/antirecall/antirecall.go
@@ -4,6 +4,7 @@ package antirecall
 import (
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/FloatTech/zbputils/control"
 	zero "github.com/wdvxdr1123/ZeroBot"
@@ -15,9 +16,10 @@ func init() {
 		DisableOnDefault: true,
 		Help:             "反闪照、反撤回",
 	})
-	engine.OnRegex(`^\[CQ:image,file=`, zero.KeywordRule("type=flash")).SetBlock(true).
+	engine.OnRegex(`^\[CQ:image.*`, zero.KeywordRule("type=flash")).SetBlock(false).
 		Handle(func(ctx *zero.Ctx) {
-			supersu := zero.BotConfig.SuperUsers[0]
+			su := zero.BotConfig.SuperUsers[0]
+			t := time.Now()
 			raw := ctx.Event.RawMessage
 			uid := ctx.Event.UserID
 			gid := ctx.Event.GroupID
@@ -25,23 +27,28 @@ func init() {
 			groupname := ctx.GetGroupInfo(gid, true).Name
 			img := strings.ReplaceAll(raw, "type=flash,", "")
 			text := message.UnescapeCQCodeText("捕捉到了一个闪照！\n" +
+				"时间:" + t.Month().String() + "." + strconv.Itoa(t.Day()) + " " + strconv.Itoa(t.Hour()) + ":" + strconv.Itoa(t.Minute()) + ":" + strconv.Itoa(t.Second()) + "\n" +
 				"来自用户:[" + username + "](" + strconv.FormatInt(uid, 10) + ")\n" +
 				"来自群聊:[" + groupname + "](" + strconv.FormatInt(gid, 10) + ")\n" +
 				"以下是原图：\n" + img)
-			ctx.SendPrivateMessage(supersu, message.ParseMessageFromString(text))
+			ctx.SendPrivateMessage(su, message.ParseMessageFromString(text))
 		})
-	engine.On("notice/group_recall").SetBlock(true).
+	engine.On("notice/group_recall").SetBlock(false).
 		Handle(func(ctx *zero.Ctx) {
-			supersu := zero.BotConfig.SuperUsers[0]
-			raw := ctx.GetMessage(ctx.Event.MessageID.(message.MessageID)).Elements.String()
+			su := zero.BotConfig.SuperUsers[0]
+			t := time.Now()
+			id := ctx.SendPrivateMessage(ctx.Event.SelfID, ctx.Event.MessageID)
+			mid := ctx.GetMessage(message.NewMessageIDFromInteger(id))
+			raw := ctx.GetMessage(message.NewMessageIDFromString(mid.Elements.String())).Elements.String()
 			uid := ctx.Event.UserID
 			gid := ctx.Event.GroupID
 			username := ctx.CardOrNickName(uid)
 			groupname := ctx.GetGroupInfo(gid, true).Name
-			text := message.UnescapeCQCodeText("捕捉到了一条消息！\n" +
+			text := message.UnescapeCQCodeText("捕捉到了一条撤回的消息！\n" +
+				"时间:" + t.Month().String() + "." + strconv.Itoa(t.Day()) + " " + strconv.Itoa(t.Hour()) + ":" + strconv.Itoa(t.Minute()) + ":" + strconv.Itoa(t.Second()) + "\n" +
 				"来自用户:[" + username + "](" + strconv.FormatInt(uid, 10) + ")\n" +
 				"来自群聊:[" + groupname + "](" + strconv.FormatInt(gid, 10) + ")\n" +
 				"以下是源消息：\n" + raw)
-			ctx.SendPrivateMessage(supersu, message.ParseMessageFromString(text))
+			ctx.SendPrivateMessage(su, message.ParseMessageFromString(text))
 		})
 }
