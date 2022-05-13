ddee3e15cb7388d0dfd5e954b41d4a670214d6da
himawari <1156544355@qq.com>
2022-05-14 02:26:41 +0800

    ✨ 添加马丁路德骂人语录

---
+++ plugin/shadiao/ergofabulous.go
@@ -0,0 +1,24 @@
+package shadiao
+
+import (
+	"github.com/FloatTech/zbputils/ctxext"
+	"github.com/antchfx/htmlquery"
+	zero "github.com/wdvxdr1123/ZeroBot"
+	"github.com/wdvxdr1123/ZeroBot/message"
+)
+
+func init() {
+	engine.OnFullMatch("马丁路德骂我").SetBlock(true).Limit(ctxext.LimitByUser).Handle(func(ctx *zero.Ctx) {
+		doc, err := htmlquery.LoadURL(ergofabulousURL)
+		if err != nil {
+			ctx.SendChain(message.Text("ERROR:", err))
+			return
+		}
+		node, err := htmlquery.Query(doc, "//main[@role=\"main\"]/p[@class=\"larger\"]/text()")
+		if err != nil {
+			ctx.SendChain(message.Text("ERROR:", err))
+			return
+		}
+		ctx.SendChain(message.Reply(ctx.Event.MessageID), message.Text(node.Data))
+	})
+}
