44387e4f5402127e3ca702b588b0fedc2dfa0ebc
Jiang-Red <79574799+Jiang-Red@users.noreply.github.com>
2022-05-14 23:50:25 +0800

    fix:saucenao插件搜图相似度判断 (#228)

--- plugin/saucenao/searcher.go
+++ plugin/saucenao/searcher.go
@@ -130,7 +130,7 @@ func init() { // 插件主体
 							})
 							resp, err := http.Head(result.Header.Thumbnail)
 							msg := make(message.Message, 0, 3)
-							if s > 0.8 {
+							if s > 80.0 {
 								msg = append(msg, message.Text("我有把握是这个!"))
 							} else {
 								msg = append(msg, message.Text("也许是这个?"))
@@ -142,7 +142,7 @@ func init() { // 插件主体
 							}
 							msg = append(msg, message.Text("\n图源: ", result.Header.IndexName, binary.BytesToString(b)))
 							ctx.Send(msg)
-							if s > 0.8 {
+							if s > 80.0 {
 								continue
 							}
 						}
