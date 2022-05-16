19ef74edf579db606cfbc633a0a1f52a0a695195
方柳煜 <101934327+fangliuyu@users.noreply.github.com>
2022-05-16 12:58:45 +0800

    Update qqmapwife.go

--- plugin/qqwife/qqmapwife.go
+++ plugin/qqwife/qqmapwife.go
@@ -1,4 +1,4 @@
-//Package qqwife 娶群友  基于“翻牌”和江林大佬的“群老婆”插件魔改作品
+// Package qqwife 娶群友  基于“翻牌”和江林大佬的“群老婆”插件魔改作品
 package qqwife
 
 import (
@@ -24,7 +24,7 @@ type 婚姻登记 struct {
 	mp map[int64]map[int64]*userinfo
 }
 
-//结婚证信息
+// 结婚证信息
 type userinfo struct {
 	target     int64  //对象身份证号
 	username   string //户主名称
@@ -120,7 +120,7 @@ func (db *婚姻登记) 登记(gid, uid, target int64, username, targetname stri
 	if !ok{
 		db.mp[gid] = make(map[int64]*userinfo, 32)
 	}
-	//填写夫妻信息
+	// 填写夫妻信息
 	uidinfo := &userinfo{
 		target:     target,
 		username:   username,
@@ -131,7 +131,7 @@ func (db *婚姻登记) 登记(gid, uid, target int64, username, targetname stri
 		username:   targetname,
 		targetname: username,
 	}
-	//民政局登记数据
+	// 民政局登记数据
 	db.mp[gid][uid] = uidinfo
 	db.mp[gid][-target] = targetinfo
 }
@@ -142,11 +142,11 @@ var (
 	skillCD  = rate.NewManager[string](time.Hour*24, 1)
 	lastdate time.Time
 	sendtext = [...][]string{
-		{ //表白成功
+		{ // 表白成功
 			"今天你向ta表白了，ta羞涩的点了点头同意了！\n",
 			"你对ta说“以我之名，冠你指间，一天相伴，一天相随”.ta捂着嘴点了点头\n\n",
 		},
-		{ //表白失败
+		{ // 表白失败
 			"今天你向ta表白了，ta毫无感情的拒绝了你",
 			"今天你向ta表白了，ta对你说“你是一个非常好的人”",
 			"今天你向ta表白了，ta给了你一个拥抱后擦肩而过",
@@ -177,7 +177,7 @@ func init() {
 			targetinfo, status, ok := 民政局.查户口(gid, uid)
 			if ok {
 				switch status {
-				case 0: //娶过别人
+				case 0: // 娶过别人
 					ctx.SendChain(
 						message.At(uid),
 						message.Text("今天你的群老婆是"),
@@ -188,7 +188,7 @@ func init() {
 							"(", targetinfo.target, ")哒",
 						),
 					)
-				default: //嫁给别人
+				default: // 嫁给别人
 					ctx.SendChain(
 						message.At(uid),
 						message.Text("今天你的群老公是"),
@@ -202,7 +202,7 @@ func init() {
 				}
 				return
 			}
-			//  无缓存获取群员列表
+			// 无缓存获取群员列表
 			temp := ctx.GetThisGroupMemberListNoCache().Array()
 			sort.SliceStable(temp, func(i, j int) bool {
 				return temp[i].Get("last_sent_time").Int() < temp[j].Get("last_sent_time").Int()
@@ -229,7 +229,7 @@ func init() {
 				ctx.SendChain(message.Text("噢, 此时此刻你还是一只单身狗, 等待下一次情缘吧"))
 				return
 			}
-			//去民政局办证
+			// 去民政局办证
 			民政局.登记(gid, uid, fiancee, ctx.CardOrNickName(uid), ctx.CardOrNickName(fiancee))
 			// 请大家吃席
 			ctx.SendChain(
@@ -243,19 +243,19 @@ func init() {
 				),
 			)
 		})
-	//单身技能
+	// 单身技能
 	engine.OnRegex(`^(娶|嫁)\[CQ:at,qq=(\d+)\]`, zero.OnlyGroup, checkdog).SetBlock(true).Limit(cdcheck, iscding).
 		Handle(func(ctx *zero.Ctx) {
 			choice := ctx.State["regex_matched"].([]string)[1]
 			fiancee, _ := strconv.ParseInt(ctx.State["regex_matched"].([]string)[2], 10, 64)
 			uid := ctx.Event.UserID
-			if uid == fiancee { //如果是自己
+			if uid == fiancee { // 如果是自己
 				ctx.SendChain(message.Text("今日获得成就：自恋狂"))
 				return
 			}
-			if rand.Intn(2) == 1 { //二分之一的概率表白成功
+			if rand.Intn(2) == 1 { // 二分之一的概率表白成功
 				gid := ctx.Event.GroupID
-				//去民政局登记
+				// 去民政局登记
 				var choicetext string
 				switch choice {
 				case "娶":
@@ -265,7 +265,7 @@ func init() {
 					民政局.登记(gid, fiancee, uid, ctx.CardOrNickName(fiancee), ctx.CardOrNickName(uid))
 					choicetext = "今天你的群老公是"
 				}
-				//请大家吃席
+				// 请大家吃席
 				ctx.SendChain(
 					message.Text(sendtext[0][rand.Intn(len(sendtext[0]))]),
 					message.At(uid),
@@ -281,7 +281,7 @@ func init() {
 			}
 			ctx.SendChain(message.Text(sendtext[1][rand.Intn(len(sendtext[1]))]))
 		})
-	//NTR技能
+	// NTR技能
 	engine.OnRegex(`^当(\[CQ:at,qq=(\d+)\] |(\d+))的小三`, zero.OnlyGroup, checkcp).SetBlock(true).Limit(cdcheck, iscding).
 		Handle(func(ctx *zero.Ctx) {
 			fid := ctx.State["regex_matched"].([]string)
@@ -292,20 +292,20 @@ func init() {
 			}
 			gid := ctx.Event.GroupID
 			uid := ctx.Event.UserID
-			//判断target是老公还是老婆
+			// 判断target是老公还是老婆
 			var choicetext string
 			targetinfo, gender, _ := 民政局.查户口(gid, fiancee)
 			switch gender{
 			case 0:
-				//让对象离婚
+				// 让对象离婚
 				民政局.离婚休妻(gid, targetinfo.target)
-				//和对象结婚登记
+				// 和对象结婚登记
 				choicetext = "老公"
 				民政局.登记(gid, fiancee, uid, ctx.CardOrNickName(fiancee), ctx.CardOrNickName(uid))
 			default:
-				//让对象离婚
+				// 让对象离婚
 				民政局.离婚休夫(gid, targetinfo.target)
-				//和对象结婚登记
+				// 和对象结婚登记
 				choicetext = "老婆"
 				民政局.登记(gid, uid, fiancee, ctx.CardOrNickName(uid), ctx.CardOrNickName(fiancee))
 			}
@@ -332,7 +332,7 @@ func init() {
 		})
 }
 
-//以群号和昵称为限制
+// 以群号和昵称为限制
 func cdcheck(ctx *zero.Ctx) *rate.Limiter {
 	limitID := strconv.FormatInt(ctx.Event.GroupID, 10) + strconv.FormatInt(ctx.Event.UserID, 10)
 	return skillCD.Load(limitID)
@@ -345,7 +345,7 @@ func iscding(ctx *zero.Ctx) {
 func checkdog(ctx *zero.Ctx) bool {
 	gid := ctx.Event.GroupID
 	if !民政局.有登记(gid) {
-		return true //如果没有人登记，说明全是单身
+		return true // 如果没有人登记，说明全是单身
 	}
 	fiancee, err := strconv.ParseInt(ctx.State["regex_matched"].([]string)[2], 10, 64)
 	if err != nil {
@@ -357,30 +357,30 @@ func checkdog(ctx *zero.Ctx) bool {
 		ctx.SendChain(message.Text("今日获得成就：自恋狂"))
 		return false
 	}
-	//获取用户info
+	// 获取用户信息
 	uidtarget, uidstatus, ok1 := 民政局.查户口(gid, uid)
 	_, fianceestatus, ok2 := 民政局.查户口(gid, fiancee)
-	if !ok1 && !ok2 { //必须是两个单身
+	if !ok1 && !ok2 { // 必须是两个单身
 		return true
 	}
-	if uidtarget.target == fiancee { //如果本就是一块
+	if uidtarget.target == fiancee { // 如果本就是一块
 		ctx.SendChain(message.Text("笨蛋~你们明明已经在一起了啊w"))
 		return false
 	}
 	if ok1 {
 		switch uidstatus {
-		case 0: //如果如为攻
+		case 0: // 如果如为攻
 			ctx.SendChain(message.Text("笨蛋~你家里还有个吃白饭的w"))
-		default: //如果为受
+		default: // 如果为受
 			ctx.SendChain(message.Text("该是0就是0，当0有什么不好"))
 		}
 		return false
 	}
 	if ok2 {
 		switch fianceestatus {
-		case 0: //如果如为攻
+		case 0: // 如果如为攻
 			ctx.SendChain(message.Text("他有别的女人了，你该放下了"))
-		default: //如果为受
+		default: // 如果为受
 			ctx.SendChain(message.Text("这是一个纯爱的世界，拒绝NTR"))
 		}
 		return false
@@ -388,31 +388,31 @@ func checkdog(ctx *zero.Ctx) bool {
 	return true
 }
 
-//注入判断 是否满足小三要求
+// 注入判断 是否满足小三要求
 func checkcp(ctx *zero.Ctx) bool {
-	//检查群内是否有人登记了
+	// 检查群内是否有人登记了
 	gid := ctx.Event.GroupID
 	if !民政局.有登记(gid) {
 		ctx.SendChain(message.Text("ta无法达成你当小三的条件"))
 		return false
 	}
-	//检查target
+	// 检查target
 	fid := ctx.State["regex_matched"].([]string)
 	fiancee, err := strconv.ParseInt(fid[2]+fid[3], 10, 64)
 	if err != nil {
 		ctx.SendChain(message.Text("额，你的对象好像不存在?"))
 		return false
 	}
-	//检查用户是否登记过
+	// 检查用户是否登记过
 	uid := ctx.Event.UserID
 	userinfo, uidstatus, ok := 民政局.查户口(gid, uid)
 	if ok {
-		if userinfo.target == fiancee { //如果本就是一块
+		if userinfo.target == fiancee { // 如果本就是一块
 			ctx.SendChain(message.Text("笨蛋~你们明明已经在一起了啊w"))
 			return false
 		}
 		switch uidstatus {
-		case 0: //如果如为攻
+		case 0: // 如果如为攻
 			ctx.SendChain(message.Text("抱歉，建国之后不支持后宫"))
 		default: //如果为受
 			ctx.SendChain(message.Text("该是0就是0，当0有什么不好"))
