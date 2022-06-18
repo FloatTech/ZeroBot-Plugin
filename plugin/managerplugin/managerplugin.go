dd2d30201b4e46b0185248e2d41ac957d618a8a4
1343396474@qq.com <1343396474@qq.com>
2022-06-18 21:07:04 +0800

    发送公告改为 触发-内容-确认

--- plugin/managerplugin/managerplugin.go
+++ plugin/managerplugin/managerplugin.go
@@ -44,37 +44,45 @@ func init() {
 			ctx.SendChain(message.ReplyWithMessage(ctx.Event.MessageID, message.Text("检测到 ["+nickname+"]("+strconv.FormatInt(ctx.Event.UserID, 10)+") 发送了干扰性消息,已处理"))...)
 			ctx.DeleteMessage(message.NewMessageIDFromInteger(ctx.Event.MessageID.(int64)))
 		})
-	engine.OnRegex(`^【.*】.*`, zero.SuperUserPermission).SetBlock(true).
+	engine.OnCommand("发送公告", zero.SuperUserPermission).SetBlock(true).
 		Handle(func(ctx *zero.Ctx) {
-			origin := ctx.Event.Message
 			next := zero.NewFutureEvent("message", 999, false, zero.OnlyGroup, ctx.CheckSession())
 			recv, stop := next.Repeat()
 			defer stop()
-			ctx.SendChain(message.Text("请输入\"确定\"或者\"取消\"来决定是否发送此公告"))
+			ctx.SendChain(message.Text("请输入公告内容"))
+			var step int
+			var origin string
 			for {
 				select {
-				case <-time.After(time.Second * 15):
+				case <-time.After(time.Second * 60):
 					ctx.SendChain(message.Text("时间太久啦！不发了！"))
 					return
 				case c := <-recv:
-					msg := c.Event.Message.ExtractPlainText()
-					if msg != "确定" && msg != "取消" {
-						ctx.SendChain(message.Text("请输入\"确定\"或者\"取消\"哟"))
-						continue
-					}
-					if msg == "确定" {
-						ctx.SendChain(message.Text("正在发送..."))
-						zero.RangeBot(func(id int64, ctx *zero.Ctx) bool {
-							for _, g := range ctx.GetGroupList().Array() {
-								gid := g.Get("group_id").Int()
-								ctx.SendGroupMessage(gid, origin)
-							}
-							return true
-						})
+					switch step {
+					case 0:
+						origin = c.Event.RawMessage
+						ctx.SendChain(message.Text("请输入\"确定\"或者\"取消\"来决定是否发送此公告"))
+						step++
+					case 1:
+						msg := c.Event.Message.ExtractPlainText()
+						if msg != "确定" && msg != "取消" {
+							ctx.SendChain(message.Text("请输入\"确定\"或者\"取消\"哟"))
+							continue
+						}
+						if msg == "确定" {
+							ctx.SendChain(message.Text("正在发送..."))
+							zero.RangeBot(func(id int64, ctx *zero.Ctx) bool {
+								for _, g := range ctx.GetGroupList().Array() {
+									gid := g.Get("group_id").Int()
+									ctx.SendGroupMessage(gid, origin)
+								}
+								return true
+							})
+							return
+						}
+						ctx.SendChain(message.Text("已经取消发送了哟~"))
 						return
 					}
-					ctx.SendChain(message.Text("已经取消发送了哟~"))
-					return
 				}
 			}
 		})
