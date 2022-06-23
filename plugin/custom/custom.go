4985401f52283561e83dfb31a92757273818ebb2
1343396474@qq.com <1343396474@qq.com>
2022-06-24 00:44:40 +0800

    更改某些功能所在插件位置

--- plugin/custom/custom.go
+++ plugin/custom/custom.go
@@ -3,6 +3,7 @@ package custom
 
 import (
 	"os"
+	"time"
 
 	ctrl "github.com/FloatTech/zbpctrl"
 	"github.com/FloatTech/zbputils/control"
@@ -15,8 +16,9 @@ func init() {
 	engine := control.Register("custom", &ctrl.Options[*zero.Ctx]{
 		DisableOnDefault: true,
 		Help: "自定义插件集合\n" +
-			" - /kill@bot\n" +
-			" - 来114514份涩图",
+			" - /kill\n" +
+			" - 来114514份涩图\n" +
+			" - /发送公告",
 	})
 	engine.OnFullMatchGroup([]string{"pause", "restart", "/kill"}, zero.OnlyToMe, zero.SuperUserPermission).SetBlock(false).
 		Handle(func(ctx *zero.Ctx) {
@@ -25,5 +27,47 @@ func init() {
 	engine.OnRegex(`^来(.*)涩图`, zero.OnlyGroup, zero.KeywordRule("114514")).SetBlock(true).
 		Handle(func(ctx *zero.Ctx) {
 			ctx.SendChain(message.Image("https://gchat.qpic.cn/gchatpic_new/1770747317/1049468946-3068097579-76A49478EFA68B4750B10B96917F7B58/0?term=3"))
+		})
+	engine.OnCommand("发送公告", zero.SuperUserPermission).SetBlock(true).
+		Handle(func(ctx *zero.Ctx) {
+			next := zero.NewFutureEvent("message", 999, false, zero.OnlyGroup, ctx.CheckSession())
+			recv, stop := next.Repeat()
+			defer stop()
+			ctx.SendChain(message.Text("请输入公告内容"))
+			var step int
+			var origin string
+			for {
+				select {
+				case <-time.After(time.Second * 60):
+					ctx.SendChain(message.Text("时间太久啦！不发了！"))
+					return
+				case c := <-recv:
+					switch step {
+					case 0:
+						origin = c.Event.RawMessage
+						ctx.SendChain(message.Text("请输入\"确定\"或者\"取消\"来决定是否发送此公告"))
+						step++
+					case 1:
+						msg := c.Event.Message.ExtractPlainText()
+						if msg != "确定" && msg != "取消" {
+							ctx.SendChain(message.Text("请输入\"确定\"或者\"取消\"哟"))
+							continue
+						}
+						if msg == "确定" {
+							ctx.SendChain(message.Text("正在发送..."))
+							zero.RangeBot(func(id int64, ctx *zero.Ctx) bool {
+								for _, g := range ctx.GetGroupList().Array() {
+									gid := g.Get("group_id").Int()
+									ctx.SendGroupMessage(gid, origin)
+								}
+								return true
+							})
+							return
+						}
+						ctx.SendChain(message.Text("已经取消发送了哟~"))
+						return
+					}
+				}
+			}
 		})
 }
--- plugin/managerplugin/managerplugin.go
+++ plugin/managerplugin/managerplugin.go
@@ -3,7 +3,6 @@ package managerplugin
 
 import (
 	"strconv"
-	"time"
 
 	zero "github.com/wdvxdr1123/ZeroBot"
 	"github.com/wdvxdr1123/ZeroBot/message"
@@ -16,7 +15,7 @@ import (
 func init() {
 	engine := control.Register("managerplugin", &ctrl.Options[*zero.Ctx]{
 		DisableOnDefault: true,
-		Help:             "自定义的群管插件\n - 开启全员禁言 群号\n - 解除全员禁言 群号\n - 踢出并拉黑 QQ号\n - 反\"XX召唤术\"\n - /发送公告",
+		Help:             "自定义的群管插件\n - 开启全员禁言 群号\n - 解除全员禁言 群号\n - 踢出并拉黑 QQ号\n - 反\"XX召唤术\"\n",
 	})
 	// 指定开启某群全群禁言 Usage: 开启全员禁言123456
 	engine.OnRegex(`^开启全员禁言.*?(\d+)`, zero.SuperUserPermission).SetBlock(true).
@@ -50,47 +49,5 @@ func init() {
 			ctx.SetGroupBan(ctx.Event.GroupID, ctx.Event.UserID, 7*24*60*60)
 			ctx.SendChain(message.ReplyWithMessage(ctx.Event.MessageID, message.Text("检测到 ["+nickname+"]("+strconv.FormatInt(ctx.Event.UserID, 10)+") 发送了干扰性消息,已处理"))...)
 			ctx.DeleteMessage(message.NewMessageIDFromInteger(ctx.Event.MessageID.(int64)))
-		})
-	engine.OnCommand("发送公告", zero.SuperUserPermission).SetBlock(true).
-		Handle(func(ctx *zero.Ctx) {
-			next := zero.NewFutureEvent("message", 999, false, zero.OnlyGroup, ctx.CheckSession())
-			recv, stop := next.Repeat()
-			defer stop()
-			ctx.SendChain(message.Text("请输入公告内容"))
-			var step int
-			var origin string
-			for {
-				select {
-				case <-time.After(time.Second * 60):
-					ctx.SendChain(message.Text("时间太久啦！不发了！"))
-					return
-				case c := <-recv:
-					switch step {
-					case 0:
-						origin = c.Event.RawMessage
-						ctx.SendChain(message.Text("请输入\"确定\"或者\"取消\"来决定是否发送此公告"))
-						step++
-					case 1:
-						msg := c.Event.Message.ExtractPlainText()
-						if msg != "确定" && msg != "取消" {
-							ctx.SendChain(message.Text("请输入\"确定\"或者\"取消\"哟"))
-							continue
-						}
-						if msg == "确定" {
-							ctx.SendChain(message.Text("正在发送..."))
-							zero.RangeBot(func(id int64, ctx *zero.Ctx) bool {
-								for _, g := range ctx.GetGroupList().Array() {
-									gid := g.Get("group_id").Int()
-									ctx.SendGroupMessage(gid, origin)
-								}
-								return true
-							})
-							return
-						}
-						ctx.SendChain(message.Text("已经取消发送了哟~"))
-						return
-					}
-				}
-			}
 		})
 }
