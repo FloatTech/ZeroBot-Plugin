f32155b6479e8f40b1254299e80bda48b89e06f9
xyzmy <108560386+xyzmy@users.noreply.github.com>
2022-07-02 12:41:46 +0800

    Update main.go

--- main.go
+++ main.go
@@ -225,7 +225,7 @@ func init() {
 
 	config.W = []*driver.WSClient{driver.NewWebSocketClient(*url, *token)}
 	config.Z = zero.Config{
-		NickName:      append([]string{*adana}, "派蒙二号" "派蒙"),
+		昵称： append（[]string{*adana}，"派蒙二号"）
 		CommandPrefix: *prefix,
 		SuperUsers:    sus,
 		Driver:        []zero.Driver{config.W[0]},
