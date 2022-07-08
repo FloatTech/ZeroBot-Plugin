41ba7aeff4ca2c2393686bfe3b64fe3be4baf785
1343396474@qq.com <1343396474@qq.com>
2022-07-08 22:22:02 +0800

    fix delreply

--- plugin/delreply/delreply.go
+++ plugin/delreply/delreply.go
@@ -12,9 +12,9 @@ import (
 func init() {
 	engine := control.Register("delreply", &ctrl.Options[*zero.Ctx]{
 		DisableOnDefault: false,
-		Help:             "回复bot自身消息\"撤回\"以撤回bot消息",
+		Help:             "回复消息\"撤回\"以撤回消息",
 	})
-	engine.OnRegex(`^\[CQ:reply,id=(.*)].*`, zero.AdminPermission, zero.KeywordRule("撤回")).SetBlock(true).
+	engine.OnRegex(`^\[CQ:reply,id=(-?[0-9]+)\].*`, zero.AdminPermission, zero.KeywordRule("撤回")).SetBlock(true).
 		Handle(func(ctx *zero.Ctx) {
 			// 获取消息id
 			mid := ctx.State["regex_matched"].([]string)[1]
