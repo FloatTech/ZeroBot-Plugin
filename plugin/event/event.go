2efe139f79db1fdca8039d8dcc2549aa30efb68d
1343396474@qq.com <1343396474@qq.com>
2022-07-18 20:14:41 +0800

    自动处理事件

--- plugin/event/event.go
+++ plugin/event/event.go
@@ -2,22 +2,66 @@
 package event
 
 import (
+	"encoding/json"
+	"os"
 	"strconv"
 	"time"
 
 	ctrl "github.com/FloatTech/zbpctrl"
 	"github.com/FloatTech/zbputils/control"
+	"github.com/FloatTech/zbputils/file"
 	"github.com/FloatTech/zbputils/process"
 	"github.com/sirupsen/logrus"
 	zero "github.com/wdvxdr1123/ZeroBot"
 	"github.com/wdvxdr1123/ZeroBot/message"
 )
 
-func init() { // 来自mayuri的插件
+type config struct {
+	AutoAcceptFriendAdd   bool `json:"AutoAcceptFriendAdd"`
+	AutoAcceptGroupInvite bool `json:"AutoAcceptGroupInvite"`
+}
+
+var (
+	cfg = config{
+		AutoAcceptFriendAdd:   false,
+		AutoAcceptGroupInvite: false,
+	}
+)
+
+func init() {
 	engine := control.Register("event", &ctrl.Options[*zero.Ctx]{
 		DisableOnDefault: false,
-		Help:             "好友申请以及群聊邀请事件处理，默认发送给主人列表第一位",
+		Help: "好友申请以及群聊邀请事件处理，默认发送给主人列表第一位\n" +
+			" - [开启|关闭]自动接受好友申请\n" +
+			" - [开启|关闭]自动接受群聊邀请",
+		PrivateDataFolder: "event",
 	})
+	path := engine.DataFolder()
+	err := os.MkdirAll(path, 0755)
+	if err != nil {
+		panic(err)
+	}
+	cfgFile := engine.DataFolder() + "config.json"
+	if file.IsExist(cfgFile) {
+		reader, err := os.Open(cfgFile)
+		if err == nil {
+			err = json.NewDecoder(reader).Decode(&cfg)
+			if err != nil {
+				panic(err)
+			}
+		} else {
+			panic(err)
+		}
+		err = reader.Close()
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		err = saveConfig(cfgFile)
+		if err != nil {
+			panic(err)
+		}
+	}
 	engine.OnRequest().SetBlock(false).
 		Handle(func(ctx *zero.Ctx) {
 			if ctx.Event.RequestType == "friend" {
@@ -27,9 +71,17 @@ func init() { // 来自mayuri的插件
 				comment := ctx.Event.Comment
 				userid := ctx.Event.UserID
 				username := ctx.CardOrNickName(userid)
-				logrus.Infoln("[manager]收到来自[", username, "](", userid, ")的好友申请")
-				id := ctx.SendPrivateMessage(
-					su,
+				logrus.Infoln("[event]收到来自[", username, "](", userid, ")的好友申请")
+				if cfg.AutoAcceptFriendAdd {
+					ctx.SetFriendAddRequest(flag, true, "")
+					ctx.SendPrivateMessage(su,
+						message.Text("已自动同意在", now, "收到来自",
+							"\n用户:[", username, "](", strconv.FormatInt(userid, 10), ")",
+							"\n的好友请求:", comment,
+							"\nflag:", flag))
+					return
+				}
+				id := ctx.SendPrivateMessage(su,
 					message.Text("在", now, "收到来自",
 						"\n用户:[", username, "](", strconv.FormatInt(userid, 10), ")",
 						"\n的好友请求:", comment,
@@ -50,9 +102,18 @@ func init() { // 来自mayuri的插件
 				invitername := ctx.CardOrNickName(inviterid)
 				groupid := ctx.Event.GroupID
 				groupname := ctx.GetGroupInfo(groupid, true).Name
-				logrus.Infoln("[manager]收到来自[", invitername, "](", inviterid, ")的群聊邀请，群:[", groupname, "](", groupid, ")")
-				id := ctx.SendPrivateMessage(
-					su,
+				logrus.Infoln("[event]收到来自[", invitername, "](", inviterid, ")的群聊邀请，群:[", groupname, "](", groupid, ")")
+				if cfg.AutoAcceptGroupInvite {
+					ctx.SetGroupAddRequest(flag, "invite", true, "")
+					ctx.SendPrivateMessage(su,
+						message.Text("已自动同意在", now, "收到来自",
+							"\n用户:[", invitername, "](", strconv.FormatInt(inviterid, 10), ")的群聊邀请",
+							"\n群聊:[", groupname, "](", strconv.FormatInt(groupid, 10), ")",
+							"\n验证信息:\n", comment,
+							"\nflag:", flag))
+					return
+				}
+				id := ctx.SendPrivateMessage(su,
 					message.Text("在", now, "收到来自",
 						"\n用户:[", invitername, "](", strconv.FormatInt(inviterid, 10), ")的群聊邀请",
 						"\n群聊:[", groupname, "](", strconv.FormatInt(groupid, 10), ")",
@@ -63,53 +124,6 @@ func init() { // 来自mayuri的插件
 				ctx.SendPrivateMessage(su, message.ReplyWithMessage(id, message.Text(flag)))
 			}
 		})
-		/*engine.OnRequest().SetBlock(false).
-		Handle(func(ctx *zero.Ctx) {
-			if ctx.Event.RequestType == "group" && ctx.Event.SubType == "invite" {
-				su := zero.BotConfig.SuperUsers[0]
-				now := time.Unix(ctx.Event.Time, 0).Format("2006-01-02 15:04:05")
-				inviterid := ctx.Event.UserID
-				invitername := ctx.CardOrNickName(inviterid)
-				groupid := ctx.Event.GroupID
-				groupname := ctx.GetGroupInfo(groupid, true).Name
-				logrus.Infoln("[manager]被用户[", invitername, "](", inviterid, ")拉至群:[", groupname, "](", groupid, ")")
-				ctx.SendPrivateMessage(
-					su,
-					message.Text("在", now,
-						"\n被用户[", invitername, "]("+strconv.FormatInt(inviterid, 10), ")拉至",
-						"\n群聊:[", groupname, "]("+strconv.FormatInt(groupid, 10), ")"))
-			}
-		})*/
-		/*engine.OnNotice().SetBlock(false).
-		Handle(func(ctx *zero.Ctx) {
-			su := zero.BotConfig.SuperUsers[0]
-			now := time.Unix(ctx.Event.Time, 0).Format("2006-01-02 15:04:05")
-			subtype := ctx.Event.SubType
-			userid := ctx.Event.UserID
-			username := ctx.CardOrNickName(userid)
-			operatorid := ctx.Event.OperatorID
-			operatorname := ctx.CardOrNickName(operatorid)
-			groupid := ctx.Event.GroupID
-			groupname := ctx.GetGroupInfo(groupid, true).Name
-			switch subtype {
-			case "kick_me":
-				{
-					ctx.SendPrivateMessage(
-						su,
-						message.Text("呜呜呜，我在", now,
-							"被[", operatorname, "](", operatorid, ")",
-							"丢出了裙[", groupname, "](", groupid, ")"))
-				}
-			case "kick":
-				{
-					ctx.SendPrivateMessage(
-						su,
-						message.Text("好可怕，[", username, "](", userid, ")",
-							"在", now,
-							"被[", operatorname, "](", operatorid, ")",
-							"丢出了裙[", groupname, "](", groupid, ")"))
-				}
-			}*/
 	engine.OnRegex(`^同意申请\s*(\d+)\s*(.*)$`, zero.SuperUserPermission, zero.OnlyPrivate).SetBlock(true).
 		Handle(func(ctx *zero.Ctx) {
 			su := zero.BotConfig.SuperUsers[0]
@@ -140,4 +154,52 @@ func init() { // 来自mayuri的插件
 			ctx.SetGroupAddRequest(flag, "invite", false, reason)
 			ctx.SendPrivateMessage(su, message.Text("已拒绝群聊邀请"))
 		})
+	engine.OnRegex(`^(.*)自动同意申请$`, zero.SuperUserPermission, zero.OnlyPrivate).SetBlock(true).
+		Handle(func(ctx *zero.Ctx) {
+			option := ctx.State["regex_matched"].([]string)[1]
+			switch option {
+			case "开启", "打开", "启用":
+				cfg.AutoAcceptFriendAdd = true
+			case "关闭", "关掉", "禁用":
+				cfg.AutoAcceptFriendAdd = false
+			default:
+				return
+			}
+			err = saveConfig(cfgFile)
+			if err == nil {
+				ctx.SendChain(message.Text("已设置自动同意好友申请为" + option))
+			} else {
+				ctx.SendChain(message.Text("ERROR:", err))
+			}
+		})
+	engine.OnRegex(`^(.*)自动同意邀请$`, zero.SuperUserPermission, zero.OnlyPrivate).SetBlock(true).
+		Handle(func(ctx *zero.Ctx) {
+			option := ctx.State["regex_matched"].([]string)[1]
+			switch option {
+			case "开启", "打开", "启用":
+				cfg.AutoAcceptGroupInvite = true
+			case "关闭", "关掉", "禁用":
+				cfg.AutoAcceptGroupInvite = false
+			default:
+				return
+			}
+			err = saveConfig(cfgFile)
+			if err == nil {
+				ctx.SendChain(message.Text("已设置自动同意群聊邀请为" + option))
+			} else {
+				ctx.SendChain(message.Text("ERROR:", err))
+			}
+		})
+}
+
+func saveConfig(cfgFile string) (err error) {
+	if reader, err := os.Create(cfgFile); err == nil {
+		err = json.NewEncoder(reader).Encode(&cfg)
+		if err != nil {
+			return err
+		}
+	} else {
+		return err
+	}
+	return nil
 }
