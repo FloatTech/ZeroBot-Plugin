2d90182810bf686fb8a041abb4b00e6eea37cac5
1343396474@qq.com <1343396474@qq.com>
2022-07-29 23:25:21 +0800

    优化代码结构

--- plugin/event/event.go
+++ plugin/event/event.go
@@ -64,13 +64,14 @@ func init() {
 	}
 	engine.OnRequest().SetBlock(false).
 		Handle(func(ctx *zero.Ctx) {
-			if ctx.Event.RequestType == "friend" {
-				su := zero.BotConfig.SuperUsers[0]
-				now := time.Unix(ctx.Event.Time, 0).Format("2006-01-02 15:04:05")
-				flag := ctx.Event.Flag
-				comment := ctx.Event.Comment
-				userid := ctx.Event.UserID
-				username := ctx.CardOrNickName(userid)
+			su := zero.BotConfig.SuperUsers[0]
+			now := time.Unix(ctx.Event.Time, 0).Format("2006-01-02 15:04:05")
+			flag := ctx.Event.Flag
+			comment := ctx.Event.Comment
+			userid := ctx.Event.UserID
+			username := ctx.CardOrNickName(userid)
+			switch ctx.Event.RequestType {
+			case "friend":
 				logrus.Infoln("[event]收到来自[", username, "](", userid, ")的好友申请")
 				if cfg.AutoAcceptFriendAdd {
 					ctx.SetFriendAddRequest(flag, true, "")
@@ -89,25 +90,18 @@ func init() {
 						"\n同意/拒绝申请，来决定同意还是拒绝"))
 				process.SleepAbout1sTo2s()
 				ctx.SendPrivateMessage(su, message.ReplyWithMessage(id, message.Text(flag)))
-			}
-		})
-	engine.OnRequest().SetBlock(false).
-		Handle(func(ctx *zero.Ctx) {
-			if ctx.Event.RequestType == "group" && ctx.Event.SubType == "invite" {
-				su := zero.BotConfig.SuperUsers[0]
-				now := time.Unix(ctx.Event.Time, 0).Format("2006-01-02 15:04:05")
-				flag := ctx.Event.Flag
-				comment := ctx.Event.Comment
-				inviterid := ctx.Event.UserID
-				invitername := ctx.CardOrNickName(inviterid)
+			case "group":
+				if ctx.Event.SubType != "invite" {
+					return
+				}
 				groupid := ctx.Event.GroupID
 				groupname := ctx.GetGroupInfo(groupid, true).Name
-				logrus.Infoln("[event]收到来自[", invitername, "](", inviterid, ")的群聊邀请，群:[", groupname, "](", groupid, ")")
-				if cfg.AutoAcceptGroupInvite {
+				logrus.Infoln("[event]收到来自[", username, "](", userid, ")的群聊邀请，群:[", groupname, "](", groupid, ")")
+				if cfg.AutoAcceptGroupInvite || isSuperUser(userid) {
 					ctx.SetGroupAddRequest(flag, "invite", true, "")
 					ctx.SendPrivateMessage(su,
 						message.Text("已自动同意在", now, "收到来自",
-							"\n用户:[", invitername, "](", strconv.FormatInt(inviterid, 10), ")的群聊邀请",
+							"\n用户:[", username, "](", strconv.FormatInt(userid, 10), ")的群聊邀请",
 							"\n群聊:[", groupname, "](", strconv.FormatInt(groupid, 10), ")",
 							"\n验证信息:\n", comment,
 							"\nflag:", flag))
@@ -115,7 +109,7 @@ func init() {
 				}
 				id := ctx.SendPrivateMessage(su,
 					message.Text("在", now, "收到来自",
-						"\n用户:[", invitername, "](", strconv.FormatInt(inviterid, 10), ")的群聊邀请",
+						"\n用户:[", username, "](", strconv.FormatInt(userid, 10), ")的群聊邀请",
 						"\n群聊:[", groupname, "](", strconv.FormatInt(groupid, 10), ")",
 						"\n验证信息:\n", comment,
 						"\n请在下方复制flag并在前面加上:",
@@ -124,35 +118,33 @@ func init() {
 				ctx.SendPrivateMessage(su, message.ReplyWithMessage(id, message.Text(flag)))
 			}
 		})
-	engine.OnRegex(`^同意申请\s*(\d+)\s*(.*)$`, zero.SuperUserPermission, zero.OnlyPrivate).SetBlock(true).
+	engine.OnRegex(`^(同意|拒绝)(申请|邀请)\s*(\d+)\s*(.*)$`, zero.SuperUserPermission, zero.OnlyPrivate).SetBlock(true).
 		Handle(func(ctx *zero.Ctx) {
 			su := zero.BotConfig.SuperUsers[0]
-			flag := ctx.State["regex_matched"].([]string)[1]
-			rename := ctx.State["regex_matched"].([]string)[2]
-			ctx.SetFriendAddRequest(flag, true, rename)
-			ctx.SendPrivateMessage(su, message.Text("已同意好友申请"))
-		})
-	engine.OnRegex(`^拒绝申请\s*(\d+)\s*(.*)$`, zero.SuperUserPermission, zero.OnlyPrivate).SetBlock(true).
-		Handle(func(ctx *zero.Ctx) {
-			su := zero.BotConfig.SuperUsers[0]
-			flag := ctx.State["regex_matched"].([]string)[1]
-			ctx.SetFriendAddRequest(flag, false, "")
-			ctx.SendPrivateMessage(su, message.Text("已拒绝好友申请"))
-		})
-	engine.OnRegex(`^同意邀请\s*(\d+)\s*(.*)$`, zero.SuperUserPermission, zero.OnlyPrivate).SetBlock(true).
-		Handle(func(ctx *zero.Ctx) {
-			su := zero.BotConfig.SuperUsers[0]
-			flag := ctx.State["regex_matched"].([]string)[1]
-			ctx.SetGroupAddRequest(flag, "invite", true, "")
-			ctx.SendPrivateMessage(su, message.Text("已同意群聊邀请"))
-		})
-	engine.OnRegex(`^拒绝邀请\s*(\d+)\s*(.*)$`, zero.SuperUserPermission, zero.OnlyPrivate).SetBlock(true).
-		Handle(func(ctx *zero.Ctx) {
-			su := zero.BotConfig.SuperUsers[0]
-			flag := ctx.State["regex_matched"].([]string)[1]
-			reason := ctx.State["regex_matched"].([]string)[2]
-			ctx.SetGroupAddRequest(flag, "invite", false, reason)
-			ctx.SendPrivateMessage(su, message.Text("已拒绝群聊邀请"))
+			cmd := ctx.State["regex_matched"].([]string)[1]
+			org := ctx.State["regex_matched"].([]string)[2]
+			flag := ctx.State["regex_matched"].([]string)[3]
+			other := ctx.State["regex_matched"].([]string)[4]
+			switch cmd {
+			case "同意":
+				switch org {
+				case "申请":
+					ctx.SetFriendAddRequest(flag, true, other)
+					ctx.SendPrivateMessage(su, message.Text("已", cmd, org))
+				}
+			case "邀请":
+				ctx.SetGroupAddRequest(flag, "invite", true, "")
+				ctx.SendPrivateMessage(su, message.Text("已", cmd, org))
+			case "拒绝":
+				switch org {
+				case "申请":
+					ctx.SetFriendAddRequest(flag, false, "")
+					ctx.SendPrivateMessage(su, message.Text("已", cmd, org))
+				case "邀请":
+					ctx.SetGroupAddRequest(flag, "invite", false, other)
+					ctx.SendPrivateMessage(su, message.Text("已", cmd, org))
+				}
+			}
 		})
 	engine.OnRegex(`^(.*)自动同意申请$`, zero.SuperUserPermission, zero.OnlyPrivate).SetBlock(true).
 		Handle(func(ctx *zero.Ctx) {
@@ -202,4 +194,13 @@ func saveConfig(cfgFile string) (err error) {
 		return err
 	}
 	return nil
+}
+
+func isSuperUser(uid int64) bool {
+	for _, su := range zero.BotConfig.SuperUsers {
+		if uid == su {
+			return true
+		}
+	}
+	return false
 }
