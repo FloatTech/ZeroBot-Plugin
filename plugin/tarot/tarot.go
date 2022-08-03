57d7dd10dca2a66d16f528d4ec10ea5ac08fd0f9
莫思敛 <55676105+shudorcl@users.noreply.github.com>
2022-08-03 17:09:15 +0800

    - 将牌阵解析加入转发列表

--- plugin/tarot/tarot.go
+++ plugin/tarot/tarot.go
@@ -165,6 +165,15 @@ func init() {
 				message.Text("\n逆位:", info.ReverseDescription))
 		} else {
 			ctx.SendChain(message.Text("没有找到", match, "噢~"))
+			// TODO 显示塔罗牌列表，下方使用图片发出，但是图片过大
+			// var build strings.Builder
+			// build.WriteString("现有塔罗牌列表\n")
+			// build.WriteString(strings.Join(formationName, " "))
+			// txt := build.String()
+			// formation, err := text.RenderToBase64(txt, text.FontFile, 400, 20)
+			// if err != nil {
+			// 	ctx.SendChain(message.Image("base64://" + binary.BytesToString(formation)))
+			// }
 		}
 	})
 	engine.OnRegex(`^((塔罗|大阿(尔)?卡纳)|小阿(尔)?卡纳|混合)牌阵\s?(.*)`, getTarot).SetBlock(true).Limit(ctxext.LimitByGroup).Handle(func(ctx *zero.Ctx) {
@@ -182,12 +191,15 @@ func init() {
 			length = 77
 		}
 		if ok {
+			ctx.SendChain(message.Text("少女祈祷中..."))
 			var build strings.Builder
 			build.WriteString(ctx.CardOrNickName(ctx.Event.UserID))
+			build.WriteString("---")
+			build.WriteString(match)
 			build.WriteString("\n")
-			msg := make([]message.MessageSegment, info.CardsNum)
+			msg := make([]message.MessageSegment, info.CardsNum+1)
 			randomIntMap := make(map[int]int, 30)
-			for i := range msg {
+			for i := 0; i < info.CardsNum; i++ {
 				j := rand.Intn(length)
 				_, ok := randomIntMap[j]
 				for ok {
@@ -198,12 +210,18 @@ func init() {
 				p := rand.Intn(2)
 				card := cardMap[(strconv.Itoa(j + start))]
 				name := card.Name
+				description := card.Description
+				if p == 1 {
+					description = card.ReverseDescription
+				}
 				tarotMsg := []message.MessageSegment{message.Image(fmt.Sprintf("%s/%s/%s", bed, reverse[p], card.ImgURL))}
 				build.WriteString(info.Represent[0][i])
 				build.WriteString(": ")
 				build.WriteString(position[p])
 				build.WriteString(" 的 ")
 				build.WriteString(name)
+				build.WriteString("\n其释义为: \n")
+				build.WriteString(description)
 				build.WriteString("\n")
 				msg[i] = ctxext.FakeSenderForwardNode(ctx, tarotMsg...)
 			}
@@ -213,8 +231,7 @@ func init() {
 				ctx.SendChain(message.Text("ERROR:", err))
 				return
 			}
-			// TODO 视gocq变化将牌阵信息加入转发列表中
-			ctx.SendChain(message.Image("base64://" + binary.BytesToString(formation)))
+			msg[info.CardsNum] = ctxext.FakeSenderForwardNode(ctx, []message.MessageSegment{message.Image("base64://" + binary.BytesToString(formation))}...)
 			ctx.SendGroupForwardMessage(ctx.Event.GroupID, msg)
 		} else {
 			ctx.SendChain(message.Text("没有找到", match, "噢~\n现有牌阵列表: ", strings.Join(formationName, " ")))
