5f7d55ff7de778df4a5fd67921f75b238e5d6673
Stardust·减 <52093397+Stardust-minus@users.noreply.github.com>
2022-08-19 00:38:13 +0800

    Update main.go

--- plugin/moegoe/main.go
+++ plugin/moegoe/main.go
@@ -46,6 +46,6 @@ func init() {
 	en.OnRegex("^让(派蒙|凯亚|安柏|丽莎|琴|香菱|枫原万叶|迪卢克|温迪|可莉|早柚|托马|芭芭拉|优菈|云堇|钟离|魈|凝光|雷电将军|北斗|甘雨|七七|刻晴|神里绫华|雷泽|神里绫人|罗莎莉亚|阿贝多|八重神子|宵宫|荒泷一斗|九条裟罗|夜兰|珊瑚宫心海|五郎|达达利亚|莫娜|班尼特|申鹤|行秋|烟绯|久岐忍|辛焱|砂糖|胡桃|重云|菲谢尔|诺艾尔|迪奥娜|鹿野院平藏')说([A-Za-z\\s\\d\u3131-\u3163\uac00-\ud7ff.。,，、:：;；!！?？]+)$").Limit(ctxext.LimitByGroup).SetBlock(true).
 		Handle(func(ctx *zero.Ctx) {
 			text := ctx.State["regex_matched"].([]string)[2]
-			id := speakers[ctx.State["regex_matched"].([]string)[1]]
+			id := ctx.State["regex_matched"].([]string)[1]
 			ctx.SendChain(message.Record(fmt.Sprintf(krapi, url.QueryEscape(text), id)))
 }
