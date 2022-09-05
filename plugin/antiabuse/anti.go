340db0c6440aaabff7c24e52e930bd5a995b8e5e
AkiraXie <l997460364@outlook.com>
2022-09-05 21:44:22 +0800

    improve antiabuse and add testcase (#398)
    * improve antiabuse and add testcase
    
    * fix managers link and add managers_test
    
    * make linter happy
    
    * add ttl.cache

--- plugin/antiabuse/anti.go
+++ plugin/antiabuse/anti.go
@@ -1,4 +1,4 @@
-// Package antiabuse defines anti_abuse plugin ,support abuse words check and add/remove abuse words
+// Package antiabuse defines antiabuse plugin ,support abuse words check and add/remove abuse words
 package antiabuse
 
 import (
@@ -14,31 +14,22 @@ import (
 )
 
 func init() {
-	engine := control.Register("anti_abuse", &ctrl.Options[*zero.Ctx]{
+	engine := control.Register("antiabuse", &ctrl.Options[*zero.Ctx]{
 		DisableOnDefault:  false,
 		Help:              "违禁词检测",
 		PrivateDataFolder: "anti_abuse",
 	})
 	onceRule := fcext.DoOnceOnSuccess(func(ctx *zero.Ctx) bool {
+		managers = ctx.State["managers"].(*ctrl.Control[*zero.Ctx]).Manager
 		db.DBPath = engine.DataFolder() + "anti_abuse.db"
 		err := db.Open(time.Hour * 4)
 		if err != nil {
 			ctx.SendChain(message.Text("open db error: ", err))
 			return false
 		}
-		err = db.Create("banUser", &banUser{})
-		if err != nil {
-			ctx.SendChain(message.Text("create table error: ", err))
-			return false
-		}
 		err = db.Create("banWord", &banWord{})
 		if err != nil {
 			ctx.SendChain(message.Text("create table error: ", err))
-			return false
-		}
-		err = recoverUser()
-		if err != nil {
-			ctx.SendChain(message.Text("recover data error: ", err))
 			return false
 		}
 		err = recoverWord()
@@ -48,29 +39,32 @@ func init() {
 		}
 		return true
 	})
-	engine.OnMessage(zero.OnlyGroup, onceRule, banRule)
+	engine.OnMessage(onceRule, zero.OnlyGroup, banRule)
 	engine.OnCommand("添加违禁词", zero.OnlyGroup, zero.AdminPermission, onceRule).Handle(
 		func(ctx *zero.Ctx) {
-			if err := insertWord(ctx.Event.GroupID, ctx.State["args"].(string)); err != nil {
-				ctx.SendChain(message.Text("add ban word error:", err))
+			args := ctx.State["args"].(string)
+			if err := insertWord(ctx.Event.GroupID, args); err != nil {
+				ctx.SendChain(message.Text("error:", err))
+			} else {
+				ctx.SendChain(message.Text(fmt.Sprintf("添加违禁词 %s 成功", args)))
 			}
 		})
 	engine.OnCommand("删除违禁词", zero.OnlyGroup, zero.AdminPermission, onceRule).Handle(
 		func(ctx *zero.Ctx) {
-			if err := deleteWord(ctx.Event.GroupID, ctx.State["args"].(string)); err != nil {
-				ctx.SendChain(message.Text("add ban word error:", err))
+			args := ctx.State["args"].(string)
+			if err := deleteWord(ctx.Event.GroupID, args); err != nil {
+				ctx.SendChain(message.Text("error:", err))
+			} else {
+				ctx.SendChain(message.Text(fmt.Sprintf("删除违禁词 %s 成功", args)))
 			}
 		})
 	engine.OnCommand("查看违禁词", zero.OnlyGroup, onceRule).Handle(
 		func(ctx *zero.Ctx) {
-			gidPrefix := fmt.Sprintf("%d-", ctx.Event.GroupID)
-			var words []string
-			_ = wordSet.Iter(func(s string) error {
-				trueWord := strings.SplitN(s, gidPrefix, 1)[1]
-				words = append(words, trueWord)
-				return nil
-			})
-			ctx.SendChain(message.Text("本群违禁词有:\n", strings.Join(words, " |")))
+			if set, ok := wordMap[ctx.Event.GroupID]; !ok {
+				ctx.SendChain(message.Text("本群无违禁词"))
+			} else {
+				ctx.SendChain(message.Text("本群违禁词有:", strings.Join(set.ToSlice(), " |")))
+			}
 		})
 
 }
