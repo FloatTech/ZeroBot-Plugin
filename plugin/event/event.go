46d5b54c58a24cfda9dc596d48f9c3e20d750577
1343396474@qq.com <1343396474@qq.com>
2022-09-07 01:00:13 +0800

    edit help

--- plugin/event/event.go
+++ plugin/event/event.go
@@ -16,7 +16,7 @@ func init() {
 	engine := control.Register("event", &ctrl.Options[*zero.Ctx]{
 		DisableOnDefault: false,
 		Help: "好友申请以及群聊邀请事件处理，默认发送给主人列表第一位\n" +
-			" - [开启|关闭]自动同意[申请|邀请|主人]\n",
+			" - [开启|关闭]自动同意[申请|邀请|主人]",
 	})
 	engine.OnRequest().SetBlock(false).
 		Handle(func(ctx *zero.Ctx) {
