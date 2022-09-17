df6957cfb6eda4c8bfa7110d1028b893b65ac143
源文雨 <41315874+fumiama@users.noreply.github.com>
2022-09-17 18:03:50 +0800

    🔖 v1.5.1-beta3

--- kanban/banner.go
+++ kanban/banner.go
@@ -9,7 +9,7 @@ import (
 var (
 	// Banner ...
 	Banner = "* OneBot + ZeroBot + Golang\n" +
-		"* Version 1.5.1-beta2 - 2022-09-11 10:32:06 +0800 CST\n" +
+		"* Version 1.5.1-beta3 - 2022-09-17 18:01:59 +0800 CST\n" +
 		"* Copyright © 2020 - 2022 FloatTech. All Rights Reserved.\n" +
 		"* Project: https://github.com/FloatTech/ZeroBot-Plugin"
 	reg = registry.NewRegReader("reilia.fumiama.top:32664", "fumiama")
