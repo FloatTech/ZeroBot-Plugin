8d8a5062292526a51346d2bc06cdf5a7ec13e21b
1343396474@qq.com <1343396474@qq.com>
2022-09-26 00:03:49 +0800

    带tag不使用缓存

--- plugin/lolicon/lolicon.go
+++ plugin/lolicon/lolicon.go
@@ -2,9 +2,10 @@
 package lolicon
 
 import (
+	"encoding/base64"
+	"errors"
 	"net/url"
 	"strings"
-	"sync"
 	"time"
 
 	"github.com/tidwall/gjson"
@@ -12,6 +13,7 @@ import (
 	"github.com/wdvxdr1123/ZeroBot/message"
 
 	"github.com/FloatTech/floatbox/math"
+	"github.com/FloatTech/floatbox/process"
 	"github.com/FloatTech/floatbox/web"
 	ctrl "github.com/FloatTech/zbpctrl"
 	"github.com/FloatTech/zbputils/control"
@@ -20,97 +22,98 @@ import (
 )
 
 const (
-	api = "https://api.lolicon.app/setu/v2"
+	api      = "https://api.lolicon.app/setu/v2"
+	capacity = 10
 )
 
-type imgpool struct {
-	pool map[string][]*message.MessageSegment
-	max  int
-	pm   sync.Mutex
-}
-
-var pool = &imgpool{
-	pool: make(map[string][]*message.MessageSegment),
-	max:  10,
-}
+var (
+	queue     = make(chan string, capacity)
+	customapi = ""
+)
 
 func init() {
 	en := control.Register("lolicon", &ctrl.Options[*zero.Ctx]{
 		DisableOnDefault: false,
 		Help: "lolicon\n" +
 			"- 随机图片\n" +
-			"- 随机图片 萝莉|少女\n",
+			"- 随机图片 萝莉|少女\n" +
+			"- 设置随机图片地址[http...]",
 	}).ApplySingle(ctxext.DefaultSingle)
 	en.OnPrefix("随机图片").Limit(ctxext.LimitByUser).SetBlock(true).
 		Handle(func(ctx *zero.Ctx) {
-			rapi := api
-			imgtype := strings.TrimSpace(ctx.State["args"].(string))
-			if imgtype != "" {
-				rapi += "?tag=" + url.QueryEscape(imgtype)
-			}
-			go pool.fill(ctx, imgtype, rapi)
-			if pool.size(imgtype) == 0 {
-				ctx.SendChain(message.Text("INFO: 正在填充弹药......"))
-				time.Sleep(time.Second * 10)
-				if pool.size(imgtype) == 0 {
-					ctx.SendChain(message.Text("ERROR: 等待填充，请稍后再试......"))
+			if imgtype := strings.TrimSpace(ctx.State["args"].(string)); imgtype != "" {
+				imageurl, err := getimgurl(api + "?tag=" + url.QueryEscape(imgtype))
+				if err != nil {
+					ctx.SendChain(message.Text("ERROR: ", err))
 					return
 				}
+				if id := ctx.Send(message.Message{ctxext.FakeSenderForwardNode(ctx, message.Image(imageurl))}).ID(); id == 0 {
+					ctx.SendChain(message.Text("ERROR: 可能被风控了"))
+				}
+				return
 			}
-			if id := ctx.Send(message.Message{ctxext.FakeSenderForwardNode(ctx, *pool.pick(imgtype))}).ID(); id == 0 {
-				ctx.SendChain(message.Text("ERROR: 可能被风控了"))
+			go func() {
+				for i := 0; i < math.Min(cap(queue)-len(queue), 2); i++ {
+					if customapi != "" {
+						data, err := web.GetData(customapi)
+						if err != nil {
+							ctx.SendChain(message.Text("ERROR: ", err))
+							continue
+						}
+						queue <- "base64://" + base64.StdEncoding.EncodeToString(data)
+						continue
+					}
+					imageurl, err := getimgurl(api)
+					if err != nil {
+						ctx.SendChain(message.Text("ERROR: ", err))
+						continue
+					}
+					name := imageurl[strings.LastIndex(imageurl, "/")+1 : len(imageurl)-4]
+					m, err := imagepool.GetImage(name)
+					if err != nil {
+						m.SetFile(imageurl)
+						_, _ = m.Push(ctxext.SendToSelf(ctx), ctxext.GetMessage(ctx))
+						process.SleepAbout1sTo2s()
+					}
+					if err == nil {
+						queue <- m.String()
+					} else {
+						queue <- imageurl
+					}
+				}
+			}()
+			select {
+			case <-time.After(time.Minute):
+				ctx.SendChain(message.Text("ERROR: 等待填充，请稍后再试......"))
+			case img := <-queue:
+				if id := ctx.Send(message.Message{ctxext.FakeSenderForwardNode(ctx, message.Image(img))}).ID(); id == 0 {
+					process.SleepAbout1sTo2s()
+					if id := ctx.Send(message.Message{ctxext.FakeSenderForwardNode(ctx, message.Image(img).Add("cache", "0"))}).ID(); id == 0 {
+						ctx.SendChain(message.Text("ERROR: 可能被风控或下载图片用时过长，请耐心等待"))
+					}
+				}
 			}
+		})
+	en.OnPrefix("设置随机图片地址", zero.SuperUserPermission).SetBlock(true).
+		Handle(func(ctx *zero.Ctx) {
+			u := strings.TrimSpace(ctx.State["args"].(string))
+			ctx.SendChain(message.Text("成功设置随机图片地址为", u))
+			customapi = u
 		})
 }
 
-func (p *imgpool) size(imgtype string) int {
-	return len(p.pool[imgtype])
-}
-
-func (p *imgpool) push(ctx *zero.Ctx, imgtype, url string) {
-	n := url[strings.LastIndex(url, "/")+1 : len(url)-4]
-	m, err := imagepool.GetImage(n)
-	var msg message.MessageSegment
+func getimgurl(url string) (string, error) {
+	data, err := web.GetData(url)
 	if err != nil {
-		m.SetFile(url)
-		_, _ = m.Push(ctxext.SendToSelf(ctx), ctxext.GetMessage(ctx))
-		msg = message.Image(m.String())
+		return "", err
 	}
-	p.pm.Lock()
-	p.pool[imgtype] = append(p.pool[imgtype], &msg)
-	p.pm.Unlock()
-}
-
-func (p *imgpool) fill(ctx *zero.Ctx, imgtype, url string) {
-	times := math.Min(p.max-p.size(imgtype), 2)
-	for i := 0; i < times; i++ {
-		data, err := web.GetData(url)
-		if err != nil {
-			ctx.SendChain(message.Text("ERROR: ", err))
-			return
-		}
-		json := gjson.ParseBytes(data)
-		if e := json.Get("error").Str; e != "" {
-			ctx.SendChain(message.Text("ERROR: ", e))
-			return
-		}
-		var imageurl string
-		if imageurl = json.Get("data.0.urls.original").Str; imageurl == "" {
-			ctx.SendChain(message.Text("未找到相关内容, 换个tag试试吧"))
-			return
-		}
-		imageurl = strings.ReplaceAll(imageurl, "i.pixiv.cat", "i.pixiv.re")
-		p.push(ctx, imgtype, imageurl)
+	json := gjson.ParseBytes(data)
+	if e := json.Get("error").Str; e != "" {
+		return "", errors.New(e)
 	}
-}
-
-func (p *imgpool) pick(imgtype string) (msg *message.MessageSegment) {
-	p.pm.Lock()
-	defer p.pm.Unlock()
-	if p.size(imgtype) == 0 {
-		return
+	var imageurl string
+	if imageurl = json.Get("data.0.urls.original").Str; imageurl == "" {
+		return "", errors.New("未找到相关内容, 换个tag试试吧")
 	}
-	msg = p.pool[imgtype][0]
-	p.pool[imgtype] = p.pool[imgtype][:1]
-	return
+	return strings.ReplaceAll(imageurl, "i.pixiv.cat", "i.pixiv.re"), nil
 }
