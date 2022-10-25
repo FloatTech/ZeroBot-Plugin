34e3aa209ee5e6d6d31e230dc6f73726718e32da
AN_PC <Genesis_AN@outlook.com>
2022-10-25 18:40:40 +0800

    change:lint补充

--- plugin/baiduaudit/audit.go
+++ plugin/baiduaudit/audit.go
@@ -19,47 +19,47 @@ import (
 // 服务网址:https://console.bce.baidu.com/ai/?_=1665977657185#/ai/antiporn/overview/index
 // 返回参数说明：https://cloud.baidu.com/doc/ANTIPORN/s/Nk3h6xbb2
 type baiduRes struct {
-	LogID          int         `json:"log_id"`         //请求唯一id
-	Conclusion     string      `json:"conclusion"`     //审核结果，可取值：合规、不合规、疑似、审核失败
-	ConclusionType int         `json:"conclusionType"` //审核结果类型，可取值1.合规，2.不合规，3.疑似，4.审核失败
+	LogID          int         `json:"log_id"`         // 请求唯一id
+	Conclusion     string      `json:"conclusion"`     // 审核结果，可取值：合规、不合规、疑似、审核失败
+	ConclusionType int         `json:"conclusionType"` // 审核结果类型，可取值1.合规，2.不合规，3.疑似，4.审核失败
 	Data           []auditData `json:"data"`
-	ErrorCode      int         `json:"error_code"` //错误提示码，失败才返回，成功不返回
-	ErrorMsg       string      `json:"error_msg"`  //错误提示信息，失败才返回，成功不返回
+	ErrorCode      int         `json:"error_code"` // 错误提示码，失败才返回，成功不返回
+	ErrorMsg       string      `json:"error_msg"`  // 错误提示信息，失败才返回，成功不返回
 }
 
 type auditData struct {
-	Type           int    `json:"type"`           //审核主类型，11：百度官方违禁词库、12：文本反作弊、13:自定义文本黑名单、14:自定义文本白名单
-	SubType        int    `json:"subType"`        //审核子类型，0:含多种类型，具体看官方链接，1:违禁违规、2:文本色情、3:敏感信息、4:恶意推广、5:低俗辱骂 6:恶意推广-联系方式、7:恶意推广-软文推广
-	Conclusion     string `json:"conclusion"`     //审核结果，可取值：合规、不合规、疑似、审核失败
-	ConclusionType int    `json:"conclusionType"` //审核结果类型，可取值1.合规，2.不合规，3.疑似，4.审核失败
-	Msg            string `json:"msg"`            //不合规项描述信息
+	Type           int    `json:"type"`           // 审核主类型，11：百度官方违禁词库、12：文本反作弊、13:自定义文本黑名单、14:自定义文本白名单
+	SubType        int    `json:"subType"`        // 审核子类型，0:含多种类型，具体看官方链接，1:违禁违规、2:文本色情、3:敏感信息、4:恶意推广、5:低俗辱骂 6:恶意推广-联系方式、7:恶意推广-软文推广
+	Conclusion     string `json:"conclusion"`     // 审核结果，可取值：合规、不合规、疑似、审核失败
+	ConclusionType int    `json:"conclusionType"` // 审核结果类型，可取值1.合规，2.不合规，3.疑似，4.审核失败
+	Msg            string `json:"msg"`            // 不合规项描述信息
 	Hits           []hit  `json:"hits"`
-} //不合规/疑似/命中白名单项详细信息。响应成功并且conclusion为疑似或不合规或命中白名单时才返回，响应失败或conclusion为合规且未命中白名单时不返回。
+} // 不合规/疑似/命中白名单项详细信息。响应成功并且conclusion为疑似或不合规或命中白名单时才返回，响应失败或conclusion为合规且未命中白名单时不返回。
 
 type hit struct {
-	DatasetName string   `json:"datasetName"`           //违规项目所属数据集名称
-	Words       []string `json:"words"`                 //送检文本命中词库的关键词（备注：建议参考新字段“wordHitPositions”，包含信息更丰富：关键词以及对应的位置及标签信息）
-	Probability float64  `json:"probability,omitempty"` //不合规项置信度
+	DatasetName string   `json:"datasetName"`           // 违规项目所属数据集名称
+	Words       []string `json:"words"`                 // 送检文本命中词库的关键词（备注：建议参考新字段“wordHitPositions”，包含信息更丰富：关键词以及对应的位置及标签信息）
+	Probability float64  `json:"probability,omitempty"` // 不合规项置信度
 } //	送检文本违规原因的详细信息
 type keyConfig struct {
-	Key1   string          `json:"key1"`   //百度云服务内容审核key存储
-	Key2   string          `json:"key2"`   //百度云服务内容审核key存储
-	Groups map[int64]group `json:"groups"` //群配置存储
+	Key1   string          `json:"key1"`   // 百度云服务内容审核key存储
+	Key2   string          `json:"key2"`   // 百度云服务内容审核key存储
+	Groups map[int64]group `json:"groups"` // 群配置存储
 }
 
 type group struct {
-	Enable             EnableMark             //是否启用内容审核
-	TextAudit          EnableMark             //文本检测
-	ImageAudit         EnableMark             //图像检测
-	DMRemind           EnableMark             //撤回提示
-	MoreRemind         EnableMark             //详细违规提示
-	DMBAN              EnableMark             //撤回后禁言
-	BANTimeAddEnable   EnableMark             //禁言累加
-	BANTime            int64                  //标准禁言时间，禁用累加，但开启禁言的的情况下采用该值
-	MaxBANTimeAddRange int64                  //最大禁言时间累加范围，最高禁言时间
-	BANTimeAddTime     int64                  //禁言累加时间，该值是开启禁累加功能后，再次触发时，根据被禁次数X该值计算出的禁言时间
-	WhiteListType      [8]bool                //类型白名单，处于白名单类型的违规，不会被触发 0:含多种类型，具体看官方链接，1:违禁违规、2:文本色情、3:敏感信息、4:恶意推广、5:低俗辱骂 6:恶意推广-联系方式、7:恶意推广-软文推广
-	AuditHistory       map[int64]auditHistory //被封禁用户列表
+	Enable             EnableMark             // 是否启用内容审核
+	TextAudit          EnableMark             // 文本检测
+	ImageAudit         EnableMark             // 图像检测
+	DMRemind           EnableMark             // 撤回提示
+	MoreRemind         EnableMark             // 详细违规提示
+	DMBAN              EnableMark             // 撤回后禁言
+	BANTimeAddEnable   EnableMark             // 禁言累加
+	BANTime            int64                  // 标准禁言时间，禁用累加，但开启禁言的的情况下采用该值
+	MaxBANTimeAddRange int64                  // 最大禁言时间累加范围，最高禁言时间
+	BANTimeAddTime     int64                  // 禁言累加时间，该值是开启禁累加功能后，再次触发时，根据被禁次数X该值计算出的禁言时间
+	WhiteListType      [8]bool                // 类型白名单，处于白名单类型的违规，不会被触发 0:含多种类型，具体看官方链接，1:违禁违规、2:文本色情、3:敏感信息、4:恶意推广、5:低俗辱骂 6:恶意推广-联系方式、7:恶意推广-软文推广
+	AuditHistory       map[int64]auditHistory // 被封禁用户列表
 }
 
 // EnableMark 启用：●，禁用：○
@@ -74,11 +74,11 @@ func (em EnableMark) String() string {
 }
 
 type auditHistory struct {
-	Count   int64      `json:"key2"`    //被禁次数
-	ResList []baiduRes `json:"reslist"` //禁言原因
+	Count   int64      `json:"key2"`    // 被禁次数
+	ResList []baiduRes `json:"reslist"` // 禁言原因
 }
 
-var bdcli *censor.ContentCensorClient //百度云审核服务Client
+var bdcli *censor.ContentCensorClient // 百度云审核服务Client
 var typetext = [8]string{
 	0: "默认违禁词库",
 	1: "违禁违规",
@@ -88,7 +88,13 @@ var typetext = [8]string{
 	5: "低俗辱骂",
 	6: "恶意推广-联系方式",
 	7: "恶意推广-软文推广",
-} //文本类型
+} // 文本类型
+
+var (
+	config     keyConfig // 插件配置
+	configinit bool      // 配置初始化
+	configpath string    // 配置路径
+)
 
 func init() {
 	engine := control.Register("baiduaudit", &ctrl.Options[*zero.Ctx]{
@@ -132,14 +138,14 @@ func init() {
 
 	engine.OnRegex("^查看检测(类型|配置)$", zero.AdminPermission, clientCheck).SetBlock(true).
 		Handle(func(ctx *zero.Ctx) {
-			//获取群配置
+			// 获取群配置
 			group := getGroup(ctx.Event.GroupID)
 			var msgs string
 			k1 := ctx.State["regex_matched"].([]string)[1]
 			if k1 == "类型" {
 				msgs += "本群检测类型:"
 				find := false
-				//遍历群检测类型名单
+				// 遍历群检测类型名单
 				for i, v := range group.WhiteListType {
 					if !v {
 						find = true
@@ -151,7 +157,7 @@ func init() {
 				}
 
 			} else {
-				//生成配置文本
+				// 生成配置文本
 				msgs = fmt.Sprintf("本群配置:\n"+
 					"内容审核:%s\n"+
 					"-文本:%s\n"+
@@ -163,7 +169,6 @@ func init() {
 					"-撤回禁言时间:%v分钟\n"+
 					"-每次累加时间:%v分钟\n"+
 					"-最大禁言时间:%v分钟", group.Enable, group.TextAudit, group.ImageAudit, group.DMRemind, group.MoreRemind, group.DMBAN, group.BANTimeAddEnable, group.BANTime, group.BANTimeAddTime, group.MaxBANTimeAddRange)
-				//fmt.Print(text)
 			}
 			b, err := text.RenderToBase64(msgs, text.FontFile, 300, 20)
 			if err != nil {
@@ -194,7 +199,6 @@ func init() {
 			k3 := ctx.State["regex_matched"].([]string)[3]
 			group := getGroup(ctx.Event.GroupID)
 			time, _ := strconv.ParseInt(k1, 10, 64)
-
 			switch k1 {
 			case "最大":
 				group.MaxBANTimeAddRange = time
@@ -249,7 +253,7 @@ func init() {
 		})
 	engine.OnMessage().SetBlock(false).Handle(func(ctx *zero.Ctx) {
 		group, ok := config.Groups[ctx.Event.GroupID]
-		//如果没该配置，或者审核功能未开启直接跳过
+		// 如果没该配置，或者审核功能未开启直接跳过
 		if !ok || !bool(group.Enable) {
 			return
 		}
@@ -322,46 +326,40 @@ func init() {
 		})
 }
 
-var (
-	config     keyConfig //插件配置
-	configinit bool      //配置初始化
-	configpath string    //配置路径
-)
-
 // 禁言检测
 func banCheck(ctx *zero.Ctx, bdres baiduRes) {
-	//如果返回类型为2（不合规），0为合规，3为疑似
+	// 如果返回类型为2（不合规），0为合规，3为疑似
 	if bdres.ConclusionType == 2 {
-		//创建消息ID
+		// 创建消息ID
 		mid := message.NewMessageIDFromInteger(ctx.Event.MessageID.(int64))
-		//获取群配置
+		// 获取群配置
 		group := getGroup(ctx.Event.GroupID)
-		//检测群配置里的不检测类型白名单，忽略掉不检测的违规类型
+		// 检测群配置里的不检测类型白名单，忽略掉不检测的违规类型
 		for i, b := range group.WhiteListType {
 			if i == bdres.Data[0].SubType && b {
 				return
 			}
 		}
-		//生成回复文本
+		// 生成回复文本
 		res := buildResp(bdres, group)
-		//撤回消息
+		// 撤回消息
 		ctx.DeleteMessage(mid)
-		//查看是否启用撤回后禁言
+		// 查看是否启用撤回后禁言
 		if group.DMBAN {
-			//从历史违规记录中获取指定用户
-			user := group.getUsder(ctx.Event.UserID)
-			//用户违规次数自增
+			// 从历史违规记录中获取指定用户
+			user := group.getUser(ctx.Event.UserID)
+			// 用户违规次数自增
 			user.Count++
-			//用户违规原因记录
+			// 用户违规原因记录
 			user.ResList = append(user.ResList, bdres)
-			//覆写该用户到群违规记录中
+			// 覆写该用户到群违规记录中
 			group.AuditHistory[ctx.Event.UserID] = user
-			//覆写该群信息
+			// 覆写该群信息
 			config.Groups[ctx.Event.GroupID] = group
-			//保存到json
+			// 保存到json
 			jsonSave(config, configpath)
 			var bantime int64
-			//查看是否开启禁言累加功能，并计算禁言时间
+			// 查看是否开启禁言累加功能，并计算禁言时间
 			if group.BANTimeAddEnable {
 				bantime = user.Count * group.BANTimeAddTime * 60
 			} else {
@@ -381,7 +379,6 @@ func banCheck(ctx *zero.Ctx, bdres baiduRes) {
 // 获取群配置
 func getGroup(groupID int64) group {
 	g, ok := config.Groups[groupID]
-	//defer jsonSave(config, configPath)
 	if ok {
 		return g
 	}
@@ -402,13 +399,12 @@ func getGroup(groupID int64) group {
 }
 
 // 从群历史违规记录中获取用户
-func (group *group) getUsder(userID int64) auditHistory {
+func (group *group) getUser(userID int64) auditHistory {
 	audit, ok := group.AuditHistory[userID]
-	//defer jsonSave(config, configPath)
 	if ok {
 		return audit
 	}
-	//如果没有用户，则创建一个并返回
+	// 如果没有用户，则创建一个并返回
 	if group.AuditHistory == nil {
 		group.AuditHistory = make(map[int64]auditHistory)
 	}
@@ -468,29 +464,29 @@ func jsonToBaiduRes(resjson string) (baiduRes, error) {
 
 // 生成回复文本
 func buildResp(bdres baiduRes, group group) []message.MessageSegment {
-	//建立消息段
+	// 建立消息段
 	msgs := make([]message.MessageSegment, 0, 8)
-	//生成简略审核结果回复
+	// 生成简略审核结果回复
 	msgs = append(msgs, message.Text(bdres.Conclusion, "\n"))
-	//查看是否开启详细审核内容提示，并确定审核内容值为疑似，或者不合规
+	// 查看是否开启详细审核内容提示，并确定审核内容值为疑似，或者不合规
 	if !group.MoreRemind {
 		return msgs
 	}
-	//遍历返回的不合规数据，生成详细违规内容
+	// 遍历返回的不合规数据，生成详细违规内容
 	for i, datum := range bdres.Data {
 		msgs = append(msgs, message.Text("[", i, "]:", datum.Msg, "\n"))
-		//检查命中词条是否大于0
+		// 检查命中词条是否大于0
 		if len(datum.Hits) == 0 {
 			return msgs
 		}
-		//遍历打印命中的违规词条
+		// 遍历打印命中的违规词条
 		for _, hit := range datum.Hits {
 			if len(datum.Hits) == 0 {
 				return msgs
 			}
 			msgs = append(msgs, message.Text("("))
 			for i4, i3 := range hit.Words {
-				//检查是否是最后一个要打印的词条，如果是则不加上逗号
+				// 检查是否是最后一个要打印的词条，如果是则不加上逗号
 				if i4 != len(hit.Words)-1 {
 					msgs = append(msgs, message.Text(i3, ","))
 				} else {
