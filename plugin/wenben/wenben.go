c4aec0e638d7b091bec40b739c04bc5fdccc8acb
lianhong <2758988938@qq.com>
2022-11-05 01:12:15 +0800

    123

--- plugin/wenben/wenben.go
+++ plugin/wenben/wenben.go
@@ -20,18 +20,18 @@ const (
 )
 
 type RspData struct {
-	Id         int    `json:"ID"`
-	Uuid       string `json:"UUID"`
-	Hitokoto   string `json:"hitokoto"`
-	Type       string `json:"type"`
-	From       string `json:"from"`
-	FromWho    string `json:"from_who"`
-	Creator    string `json:"creator"`
-	CreatorUid int    `json:"creator_UID"`
-	Reviewer   int    `json:"reviewer"`
-	CommitFrom string `json:"commit_from"`
-	CreatedAt  string `json:"created_at"`
-	Length     int    `json:"length"`
+	ID          int    `json:"ID"`
+	UUID        string `json:"UUID"`
+	Hitokoto    string `json:"hitokoto"`
+	Type        string `json:"type"`
+	From        string `json:"from"`
+	FromWho     string `json:"from_who"`
+	Creator     string `json:"creator"`
+	creator_UID int    `json:"creator_UID"`
+	Reviewer    int    `json:"reviewer"`
+	CommitFrom  string `json:"commit_from"`
+	CreatedAt   string `json:"created_at"`
+	Length      int    `json:"length"`
 }
 
 func init() { // 主函数
