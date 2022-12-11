ed518afa990c4826e6a3ee90c20a65c2f4b7bb27
方柳煜 <101934327+fangliuyu@users.noreply.github.com>
2022-12-11 14:02:13 +0800

    "Update"

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 go version
 go env -w GOPROXY=https://goproxy.cn,direct
 go env -w GO111MODULE=auto
-go mod tidy
-#go build -ldflags="-s -w" -o ZeroBot-Plugin
 while true
 do
+    go mod tidy
+    #go build -ldflags="-s -w" -o ZeroBot-Plugin
     go run main.go -c config.json
 done
