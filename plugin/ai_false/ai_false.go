1795b9196b7bd25f387d82f18f5542ecfec21193
源文雨 <41315874+fumiama@users.noreply.github.com>
2023-02-19 20:03:33 +0800

    fix: 自检

--- plugin/ai_false/ai_false.go
+++ plugin/ai_false/ai_false.go
@@ -141,7 +141,7 @@ func drawstatus(m *ctrl.Control[*zero.Ctx], uid int64, botname string) (sendimg 
 	}
 
 	dldata := *(**[]byte)(atomic.LoadPointer((*unsafe.Pointer)(unsafe.Pointer(&bgdata))))
-	if dldata == (*[]byte)(nil) || uintptr(time.Since(boottime).Hours())/24 >= atomic.LoadUintptr(&bgcount) {
+	if dldata == (*[]byte)(nil) || uintptr(time.Since(boottime).Hours()/24) >= atomic.LoadUintptr(&bgcount) {
 		url, err1 := bilibili.GetRealURL(backgroundURL)
 		if err1 != nil {
 			return nil, err1
