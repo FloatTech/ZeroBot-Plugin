091d3170fca0363abc944798ad09f12cc015822f
himawari <54976075+guohuiyuan@users.noreply.github.com>
2023-02-25 19:48:13 +0800

    Feature zbpp bilibilipush 20230224 (#599)
    * 🔥 删除小鸡词典
    
    * ✨ 添加新插件和修改bilibilipush问题
    
    * 🎨 去掉多余结构
    
    * 🎨 简化语句

---
+++ plugin/dailynews/dailynews.go
@@ -0,0 +1,37 @@
+// Package dailynews 今日早报
+package dailynews
+
+import (
+	"github.com/FloatTech/floatbox/binary"
+	"github.com/FloatTech/floatbox/web"
+	ctrl "github.com/FloatTech/zbpctrl"
+	"github.com/FloatTech/zbputils/control"
+	"github.com/tidwall/gjson"
+	zero "github.com/wdvxdr1123/ZeroBot"
+	"github.com/wdvxdr1123/ZeroBot/message"
+)
+
+const api = "http://dwz.2xb.cn/zaob"
+
+func init() {
+	engine := control.Register("dailynews", &ctrl.Options[*zero.Ctx]{
+		DisableOnDefault:  false,
+		Brief:             "今日早报",
+		Help:              "- 今日早报",
+		PrivateDataFolder: "dailynews",
+	})
+
+	engine.OnFullMatch(`今日早报`).SetBlock(true).
+		Handle(func(ctx *zero.Ctx) {
+			data, err := web.GetData(api)
+			if err != nil {
+				return
+			}
+			picURL := gjson.Get(binary.BytesToString(data), "imageUrl").String()
+			if err != nil {
+				ctx.SendChain(message.Text("ERROR:", err))
+				return
+			}
+			ctx.SendChain(message.Image(picURL))
+		})
+}
