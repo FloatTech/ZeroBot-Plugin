4dc837854b0c3f80d177fda273c50d397a6fa371
源文雨 <41315874+fumiama@users.noreply.github.com>
2023-03-01 11:11:01 +0800

    fix windows console input

--- main_windows.go
+++ main_windows.go
@@ -21,6 +21,15 @@ func init() {
 	mode &^= windows.ENABLE_QUICK_EDIT_MODE // 禁用快速编辑模式
 	mode |= windows.ENABLE_EXTENDED_FLAGS   // 启用扩展标志
 
+	mode &^= windows.ENABLE_MOUSE_INPUT    // 禁用鼠标输入
+	mode |= windows.ENABLE_PROCESSED_INPUT // 启用控制输入
+
+	mode &^= windows.ENABLE_INSERT_MODE                           // 禁用插入模式
+	mode |= windows.ENABLE_ECHO_INPUT | windows.ENABLE_LINE_INPUT // 启用输入回显&逐行输入
+
+	mode &^= windows.ENABLE_WINDOW_INPUT           // 禁用窗口输入
+	mode &^= windows.ENABLE_VIRTUAL_TERMINAL_INPUT // 禁用虚拟终端输入
+
 	err = windows.SetConsoleMode(stdin, mode)
 	if err != nil {
 		panic(err)
