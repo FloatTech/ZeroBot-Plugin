ca3ff1b522fe074c6cb66b6f5187666310437ea7
源文雨 <41315874+fumiama@users.noreply.github.com>
2023-03-01 11:46:00 +0800

    fix: set title

--- console/console_windows.go
+++ console/console_windows.go
@@ -29,7 +29,7 @@ func setConsoleTitle(title string) (err error) {
 	if err != nil {
 		return
 	}
-	r1, _, e1 := syscall.Syscall(procSetConsoleTitle.Addr(), 1, uintptr(unsafe.Pointer(p0)), 0)
+	r1, _, e1 := syscall.Syscall(procSetConsoleTitle.Addr(), 1, uintptr(unsafe.Pointer(p0)), 0, 0)
 	if r1 == 0 {
 		err = errnoErr(e1)
 	}
