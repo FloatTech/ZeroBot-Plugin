b002c8b5f0422491d5855031fabb395b2e2b6499
方柳煜 <101934327+fangliuyu@users.noreply.github.com>
2023-03-15 22:43:50 +0800

    Update main.go

--- plugin/drawlots/main.go
+++ plugin/drawlots/main.go
@@ -46,7 +46,7 @@ var (
 		Help: "支持图包文件夹和gif抽签\n" +
 			"-------------\n" +
 			"- (刷新)抽签列表\n- 抽[签名]签\n- 看[gif签名]签\n- 加[签名]签[gif图片]\n- 删[gif签名]签",
-		PublicDataFolder: "DrawLots",
+		PrivateDataFolder: "drawlots",
 	}).ApplySingle(ctxext.DefaultSingle)
 	datapath = file.BOTPATH + "/" + en.DataFolder()
 )
