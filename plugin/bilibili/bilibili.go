9db491f1b1e8437f962c1392f4ee3169c87f8810
DreamZero <79574799+Jiang-Red@users.noreply.github.com>
2023-03-15 16:00:56 +0000

    newline

--- plugin/bilibili/bilibili.go
+++ plugin/bilibili/bilibili.go
@@ -56,7 +56,7 @@ func init() {
 			"- 查成分 [xxx]\n" +
 			"- 查弹幕 [xxx]\n" +
 			"- 设置b站cookie b_ut=7;buvid3=0;i-wanna-go-back=-1;innersign=0;\n" +
-			"- 更新vup" +
+			"- 更新vup\n" +
 			"Tips: (412就是拦截的意思,建议私聊把cookie设全)\n",
 		PublicDataFolder: "Bilibili",
 	})
