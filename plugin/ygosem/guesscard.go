1af1e98172eebf6d31dda42dc436ff1276b05e84
方柳煜 <101934327+fangliuyu@users.noreply.github.com>
2023-04-14 20:17:07 +0800

    Fix

--- plugin/ygosem/guesscard.go
+++ plugin/ygosem/guesscard.go
@@ -485,7 +485,7 @@ func (sql *carddb) loadpunish(gid int64, i int) error {
 	err := sql.db.Create("punish", &punish{})
 	if err == nil {
 		var groupInfo punish
-		_ = sql.db.Find("punish", &groupInfo, "where GroupID is "+strconv.FormatInt(gid, 10))
+		_ = sql.db.Find("punish", &groupInfo, "where GroupID = "+strconv.FormatInt(gid, 10))
 		groupInfo.GroupID = gid
 		groupInfo.LastTime = time.Now().Unix()
 		groupInfo.Value += i
@@ -501,8 +501,8 @@ func (sql *carddb) checkGroup(gid int64) (float64, bool) {
 	if err != nil {
 		return 0, true
 	}
-	groupInfo := new(punish)
-	_ = sql.db.Find("punish", &groupInfo, "where GroupID is "+strconv.FormatInt(gid, 10))
+	var groupInfo punish
+	_ = sql.db.Find("punish", &groupInfo, "where GroupID = "+strconv.FormatInt(gid, 10))
 	if groupInfo.LastTime > 0 {
 		subTime := time.Since(time.Unix(groupInfo.LastTime, 0)).Minutes()
 		if subTime >= 30 {
