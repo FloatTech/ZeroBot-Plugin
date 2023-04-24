c9169913140cbd019fd373d79a9a2a25884aecda
lianhong <2758988938@qq.com>
2023-04-24 20:35:45 +0800

    fix

--- plugin/kokomi/kokomi.go
+++ plugin/kokomi/kokomi.go
@@ -12,11 +12,11 @@ import (
 	"github.com/FloatTech/zbputils/control"
 	zero "github.com/wdvxdr1123/ZeroBot"
 	"github.com/wdvxdr1123/ZeroBot/message"
+	"github.com/wdvxdr1123/ZeroBot/utils/helper"
 )
 
 const (
-	api     = "http://8.134.179.136/"
-	genshin = "genshin/"
+	api = "http://8.134.179.136/genshin/"
 )
 
 func init() {
@@ -27,12 +27,12 @@ func init() {
 	})
 	en.OnRegex(`^(?:#|＃)?\s*绑定+?\s*(?:uid|UID|Uid)?\s*(\d+)?`).SetBlock(true).Handle(func(ctx *zero.Ctx) {
 		suid := ctx.State["regex_matched"].([]string)[1] // 获取uid
-		body, err := getData(api + genshin + "bound?qq=" + strconv.Itoa(int(ctx.Event.UserID)) + "&uid=" + suid)
+		body, err := getData(api + "bound?qq=" + strconv.Itoa(int(ctx.Event.UserID)) + "&uid=" + suid)
 		if err != nil {
-			ctx.SendChain(message.Text("ERROR: ", string(body), err))
+			ctx.SendChain(message.Text("ERROR: ", helper.BytesToString(body), err))
 			return
 		}
-		ctx.SendChain(message.Text(string(body)))
+		ctx.SendChain(message.Text(helper.BytesToString(body)))
 	})
 	en.OnRegex(`^(?:#|＃)?(.*)面板\s*(?:(?:\[CQ:at,qq=)(\d+))?(\d+)?(.*)`).SetBlock(true).Handle(func(ctx *zero.Ctx) {
 		var i string
@@ -45,16 +45,16 @@ func init() {
 				i = strconv.FormatInt(ctx.Event.UserID, 10)
 			}
 			if str == "更新" {
-				body, err := getData(api + genshin + "find?qq=" + i)
+				body, err := getData(api + "find?qq=" + i)
 				if err != nil {
-					ctx.SendChain(message.Text("ERROR: ", string(body), err))
+					ctx.SendChain(message.Text("ERROR: ", helper.BytesToString(body), err))
 					return
 				}
-				ctx.SendChain(message.Text(string(body)))
+				ctx.SendChain(message.Text(helper.BytesToString(body)))
 			} else {
-				body, err := getData(api + genshin + "qtop?qq=" + i + "&role=" + str)
+				body, err := getData(api + "qtop?qq=" + i + "&role=" + str)
 				if err != nil {
-					ctx.SendChain(message.Text("ERROR: ", string(body), err))
+					ctx.SendChain(message.Text("ERROR: ", helper.BytesToString(body), err))
 					return
 				}
 				ctx.SendChain(message.ImageBytes(body))
@@ -63,29 +63,29 @@ func init() {
 		}
 		i = ctx.State["regex_matched"].([]string)[3]
 		if str == "更新" {
-			body, err := getData(api + genshin + "find?uid=" + i)
+			body, err := getData(api + "find?uid=" + i)
 			if err != nil {
-				ctx.SendChain(message.Text("ERROR: ", string(body), err))
+				ctx.SendChain(message.Text("ERROR: ", helper.BytesToString(body), err))
 				return
 			}
-			ctx.SendChain(message.Text(string(body)))
+			ctx.SendChain(message.Text(helper.BytesToString(body)))
 			return
 		}
-		body, err := getData(api + genshin + "utop?uid=" + i + "&role=" + str)
+		body, err := getData(api + "utop?uid=" + i + "&role=" + str)
 		if err != nil {
-			ctx.SendChain(message.Text("ERROR: ", string(body), err))
+			ctx.SendChain(message.Text("ERROR: ", helper.BytesToString(body), err))
 			return
 		}
 		ctx.SendChain(message.ImageBytes(body))
 	})
 	en.OnRegex(`^(?:#|＃)?\s*更新+?\s*(?:uid|UID|Uid)?\s*(\d+)?`).SetBlock(true).Handle(func(ctx *zero.Ctx) {
 		suid := ctx.State["regex_matched"].([]string)[1] // 获取uid
-		body, err := getData(api + genshin + "find?uid=" + suid)
+		body, err := getData(api + "find?uid=" + suid)
 		if err != nil {
-			ctx.SendChain(message.Text("ERROR: ", string(body), err))
+			ctx.SendChain(message.Text("ERROR: ", helper.BytesToString(body), err))
 			return
 		}
-		ctx.SendChain(message.Text(string(body)))
+		ctx.SendChain(message.Text(helper.BytesToString(body)))
 	})
 }
 
@@ -94,9 +94,12 @@ func getData(url string) (data []byte, err error) {
 	var response *http.Response
 	response, err = http.Get(url)
 	if err == nil {
-		if response.StatusCode != http.StatusOK {
-			s := fmt.Sprintf("\ncode: %d", response.StatusCode)
+		if response.StatusCode != http.StatusOK && response.StatusCode != http.StatusInternalServerError {
+			s := fmt.Sprintf("status code: %d", response.StatusCode)
 			err = errors.New(s)
+			return
+		} else if response.StatusCode == http.StatusInternalServerError {
+			err = errors.New("\n-----------------------------")
 		}
 		data, _ = io.ReadAll(response.Body)
 		response.Body.Close()
