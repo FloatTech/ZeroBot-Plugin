0c9316b1eaadb69f5c0702bce2b2063e66aea309
Papklp <66307812+Papklp@users.noreply.github.com>
2023-11-02 19:32:40 +0800

    Update main.go

--- main.go
+++ main.go
@@ -222,11 +222,11 @@ func init() {
 	h := flag.Bool("h", false, "Display this help.")
 	// g := flag.String("g", "127.0.0.1:3000", "Set webui url.")
 	// 直接写死 AccessToken 时，请更改下面第二个参数
-	token := flag.String("t", "", "Set AccessToken of WSClient.")
+	token := flag.String("t", "114514", "Set AccessToken of WSClient.")
 	// 直接写死 URL 时，请更改下面第二个参数
-	url := flag.String("u", "ws://127.0.0.1:6700", "Set Url of WSClient.")
+	url := flag.String("u", "ws://172.18.0.6:8082", "Set Url of WSClient.")
 	// 默认昵称
-	adana := flag.String("n", "椛椛", "Set default nickname.")
+	adana := flag.String("n", "Niko", "Set default nickname.")
 	prefix := flag.String("p", "/", "Set command prefix.")
 	runcfg := flag.String("c", "", "Run from config file.")
 	save := flag.String("s", "", "Save default config to file and exit.")
@@ -258,7 +258,7 @@ func init() {
 	}
 
 	// 通过代码写死的方式添加主人账号
-	// sus = append(sus, 12345678)
+	 sus = append(sus, 2115505395)
 	// sus = append(sus, 87654321)
 
 	// 启用 webui
