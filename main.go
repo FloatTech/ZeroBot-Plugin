cfdb6e6528c8f4797c2eccb9bc2a02d3c8046378
AEsri-Dream <149231217+AEsri-Dream@users.noreply.github.com>
2023-11-05 14:23:39 +0800

    Update main.go

--- main.go
+++ main.go
@@ -78,6 +78,7 @@ import (
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/choose"           // 选择困难症帮手
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/chouxianghua"     // 说抽象话
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/chrev"            // 英文字符翻转
+        _ "github.com/FloatTech/ZeroBot-Plugin/plugin/cybercat"            // 云养猫
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/coser"            // 三次元小姐姐
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/cpstory"          // cp短打
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/dailynews"        // 今日早报
