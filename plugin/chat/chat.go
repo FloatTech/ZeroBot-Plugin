3eb63b0eb51b918f439754c8c8f59804716f0125
Papklp <66307812+Papklp@users.noreply.github.com>
2023-11-07 23:05:02 +0800

    Update chat.go

--- plugin/chat/chat.go
+++ plugin/chat/chat.go
@@ -46,11 +46,11 @@ func init() { // 插件主体
 				// 5分钟共8块命令牌 一次消耗3块命令牌
 				time.Sleep(time.Second * 1)
 				ctx.SendChain(message.Text("请不要戳", nickname, " >_<"))
-			case poke.Load(ctx.Event.GroupID).AcquireN(3):
+			case poke.Load(ctx.Event.GroupID).AcquireM(3):
 				// 5分钟共8块命令牌 一次消耗3块命令牌
 				time.Sleep(time.Second * 1)
 				ctx.SendChain(message.Text("你戳你妈呢😅"))
-			case poke.Load(ctx.Event.GroupID).Acquire():
+			case poke.Load(ctx.Event.GroupID).Acquire(1):
 				// 5分钟共8块命令牌 一次消耗1块命令牌
 				time.Sleep(time.Second * 1)
 				ctx.SendChain(message.Text("你妈死了捏😋"))
