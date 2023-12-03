d4331dcadf80d52431469528f8abc77ed5b68e79
东经雨 <114856851+Dong8jing8yu@users.noreply.github.com>
2023-12-03 13:56:14 +0800

    Update main.go

--- main.go
+++ main.go
@@ -258,8 +258,8 @@ func init() {
 	}
 
 	// 通过代码写死的方式添加主人账号
-	 sus = append(sus, 1055411737)
-	// sus = append(sus, 87654321)
+	   sus = append(sus, 1055411737)
+	   sus = append(sus, 3236800016)
 
 	// 启用 webui
 	// go webctrl.RunGui(*g)
