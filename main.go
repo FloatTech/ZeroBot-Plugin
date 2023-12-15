c69fbcbe76a89b32c28a1e702b7418af65730312
oh4emp <100841541+oh4emp@users.noreply.github.com>
2023-12-15 21:49:47 +0800

    Update main.go

--- main.go
+++ main.go
@@ -226,7 +226,7 @@ func init() {
 	// 直接写死 URL 时，请更改下面第二个参数
 	url := flag.String("u", "ws://127.0.0.1:6700", "Set Url of WSClient.")
 	// 默认昵称
-	adana := flag.String("n", "椛椛", "Set default nickname.")
+	adana := flag.String("n", "青喵", "Set default nickname.")
 	prefix := flag.String("p", "/", "Set command prefix.")
 	runcfg := flag.String("c", "", "Run from config file.")
 	save := flag.String("s", "", "Save default config to file and exit.")
@@ -258,8 +258,8 @@ func init() {
 	}
 
 	// 通过代码写死的方式添加主人账号
-	// sus = append(sus, 12345678)
-	// sus = append(sus, 87654321)
+	// sus = append(sus, 2243363336)
+	// sus = append(sus, 928003739)
 
 	// 启用 webui
 	// go webctrl.RunGui(*g)
