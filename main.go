fc269f1354398c9b0e08da05291cf03916cfe3da
asczsc <156320234+asczsc@users.noreply.github.com>
2024-01-12 22:04:22 +0800

    Update main.go

--- main.go
+++ main.go
@@ -258,8 +258,8 @@ func init() {
 	}
 
 	// 通过代码写死的方式添加主人账号
-	// sus = append(sus, 12345678)
-	// sus = append(sus, 87654321)
+	 //sus = append(sus, 1684984605)
+	 sus = append(sus, 2284018021)
 
 	// 启用 webui
 	// go webctrl.RunGui(*g)
