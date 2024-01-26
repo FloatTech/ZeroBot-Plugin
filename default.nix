3ee1b08d70783abddcfefb8161aaa832251bf59e
anonymous <anonymous@anonymous.anonymous44>
2024-01-26 01:07:31 +0000

    nix fmt

--- default.nix
+++ default.nix
@@ -1,17 +1,17 @@
-{ pkgs ? (
+{
+  pkgs ? (
     let
       inherit (builtins) fetchTree fromJSON readFile;
       inherit ((fromJSON (readFile ./flake.lock)).nodes) nixpkgs gomod2nix;
     in
-    import (fetchTree nixpkgs.locked) {
-      overlays = [
-        (import "${fetchTree gomod2nix.locked}/overlay.nix")
-      ];
-    }
-  )
-, buildGoApplication ? pkgs.buildGoApplication
+      import (fetchTree nixpkgs.locked) {
+        overlays = [
+          (import "${fetchTree gomod2nix.locked}/overlay.nix")
+        ];
+      }
+  ),
+  buildGoApplication ? pkgs.buildGoApplication,
 }:
-
 buildGoApplication {
   pname = "ZeroBot-Plugin";
   version = "0.1";
