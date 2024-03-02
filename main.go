ef8d54a63025ae7e20730394c404d9338e7d6137
nitlei <161731456+nitlei@users.noreply.github.com>
2024-03-02 23:32:38 +0800

    Update main.go

--- main.go
+++ main.go
@@ -224,7 +224,7 @@ func init() {
 	// 直接写死 AccessToken 时，请更改下面第二个参数
 	token := flag.String("t", "", "Set AccessToken of WSClient.")
 	// 直接写死 URL 时，请更改下面第二个参数
-	url := flag.String("u", "ws://127.0.0.1:6700", "Set Url of WSClient.")
+	url := flag.String("u", "ws://211.90.236.232:5800", "Set Url of WSClient.")
 	// 默认昵称
 	adana := flag.String("n", "椛椛", "Set default nickname.")
 	prefix := flag.String("p", "/", "Set command prefix.")
