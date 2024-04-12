227534759c6080fa576d432d946c988a3a4ac7a5
SMDsm2 <166809834+SMDsm2@users.noreply.github.com>
2024-04-12 17:49:27 +0800

    Update main.go

--- main.go
+++ main.go
@@ -225,7 +225,7 @@ func init() {
 	// 直接写死 URL 时，请更改下面第二个参数
 	url := flag.String("u", "ws://127.0.0.1:6700", "Set Url of WSClient.")
 	// 默认昵称
-	adana := flag.String("n", "椛椛", "Set default nickname.")
+	adana := flag.String("n", "伊蕾娜", "Set default nickname.")
 	prefix := flag.String("p", "/", "Set command prefix.")
 	runcfg := flag.String("c", "", "Run from config file.")
 	save := flag.String("s", "", "Save default config to file and exit.")
@@ -257,7 +257,7 @@ func init() {
 	}
 
 	// 通过代码写死的方式添加主人账号
-	// sus = append(sus, 12345678)
+	// sus = append(sus, 2558968454)
 	// sus = append(sus, 87654321)
 
 	// 启用 webui
