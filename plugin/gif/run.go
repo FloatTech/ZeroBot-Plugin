56c3a5546173426054b1ffae32b3476c122aa2d0
vatebur <vatebur@gmail.com>
2024-05-09 15:08:42 +0800

    [issues 900] Update gif plugin for image cache
    [issues 900] Update gif plugin for image cache
    修改gif插件图片保存方式

--- plugin/gif/run.go
+++ plugin/gif/run.go
@@ -152,8 +152,9 @@ func init() { // 插件主体
 	datapath = file.BOTPATH + "/" + en.DataFolder()
 	en.OnRegex(`^(` + strings.Join(cmd, "|") + `)[\s\S]*?(\[CQ:(image\,file=([0-9a-zA-Z]{32}).*|at.+?(\d{5,11}))\].*|(\d+))$`).
 		SetBlock(true).Handle(func(ctx *zero.Ctx) {
-		c := newContext(ctx.Event.UserID)
 		list := ctx.State["regex_matched"].([]string)
+		atUserID, _ := strconv.ParseInt(list[4]+list[5]+list[6], 10, 64)
+		c := newContext(ctx.Event.UserID, atUserID)
 		err := c.prepareLogos(list[4]+list[5]+list[6], strconv.FormatInt(ctx.Event.UserID, 10))
 		if err != nil {
 			ctx.SendChain(message.Text("ERROR: ", err))
