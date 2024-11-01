f5e1c197ddaa97385eb4cc557662f9081a88105c
源文雨 <41315874+fumiama@users.noreply.github.com>
2024-11-01 14:55:01 +0900

    feat: add custom folder

---
+++ custom/doc.go
@@ -0,0 +1,2 @@
+// Package custom 注册用户自定义插件于此
+package custom
--- main.go
+++ main.go
@@ -62,6 +62,7 @@ import (
 	//                          vvvvvvvvvvvvvv                          //
 	//                               vvvv                               //
 
+	_ "github.com/FloatTech/ZeroBot-Plugin/custom"                  // 自定义插件合集
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/ahsai"            // ahsai tts
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/aifalse"          // 服务器监控
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/aiwife"           // 随机老婆
