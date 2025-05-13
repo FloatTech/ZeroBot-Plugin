d22c253d9c6aeb74b3d893b519d8a3afc371ed9b
Dodoj <31199261+Doordoorjay@users.noreply.github.com>
2025-05-13 09:11:34 +0000

    Fix lint

--- plugin/wordcount/main.go
+++ plugin/wordcount/main.go
@@ -26,7 +26,7 @@ var (
 	seg                 gse.Segmenter
 )
 
-// MessageRecord将保存聊天消息的时间与内容到json
+// MessageRecord represents a chat message with timestamp and text to json.
 type MessageRecord struct {
 	Time int64  `json:"time"`
 	Text string `json:"text"`
