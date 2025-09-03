8bfee1a4ca5f42906923919e75f85ce7aaf671f7
小锅饭 <1156544355@qq.com>
2025-09-03 14:25:26 +0800

    ✨ 添加语音

--- plugin/crypter/handlers.go
+++ plugin/crypter/handlers.go
@@ -2,6 +2,8 @@
 package crypter
 
 import (
+	"github.com/FloatTech/AnimeAPI/airecord"
+	"github.com/sirupsen/logrus"
 	zero "github.com/wdvxdr1123/ZeroBot"
 	"github.com/wdvxdr1123/ZeroBot/message"
 )
@@ -10,7 +12,14 @@ import (
 func houEncryptHandler(ctx *zero.Ctx) {
 	text := ctx.State["regex_matched"].([]string)[1]
 	result := encodeHou(text)
-	ctx.SendChain(message.Text(result))
+	logrus.Infoln("[crypter] 回复内容:", result)
+	recCfg := airecord.GetConfig()
+	record := ctx.GetAIRecord(recCfg.ModelID, recCfg.Customgid, result)
+	if record != "" {
+		ctx.SendChain(message.Record(record))
+	} else {
+		ctx.SendChain(message.Text(result))
+	}
 }
 
 func houDecryptHandler(ctx *zero.Ctx) {
