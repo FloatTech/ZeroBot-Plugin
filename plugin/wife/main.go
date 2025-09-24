7bd1653cb5206a3181944d5edec029cc28824c22
莫思潋 <55676105+shudorcl@users.noreply.github.com>
2025-09-25 00:38:33 +0800

    feat(wife): 增加作品名&猜老婆 (#1208)

--- plugin/wife/main.go
+++ plugin/wife/main.go
@@ -3,8 +3,9 @@ package wife
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
-	"strings"
+	"regexp"
 
 	fcext "github.com/FloatTech/floatbox/ctxext"
 	ctrl "github.com/FloatTech/zbpctrl"
@@ -15,15 +16,27 @@ import (
 	"github.com/wdvxdr1123/ZeroBot/message"
 )
 
-func init() {
-	engine := control.AutoRegister(&ctrl.Options[*zero.Ctx]{
+var (
+	cards  = []string{}
+	re     = regexp.MustCompile(`^\[(.*?)\](.*)\..*$`)
+	engine = control.AutoRegister(&ctrl.Options[*zero.Ctx]{
 		DisableOnDefault: false,
 		Help:             "- 抽老婆",
 		Brief:            "从老婆库抽每日老婆",
 		PublicDataFolder: "Wife",
 	}).ApplySingle(ctxext.DefaultSingle)
+)
+
+func card2name(card string) (string, string) {
+	match := re.FindStringSubmatch(card)
+	if len(match) >= 3 {
+		return match[1], match[2]
+	}
+	return "", ""
+}
+
+func init() {
 	_ = os.MkdirAll(engine.DataFolder()+"wives", 0755)
-	cards := []string{}
 	engine.OnFullMatch("抽老婆", fcext.DoOnceOnSuccess(
 		func(ctx *zero.Ctx) bool {
 			data, err := engine.GetLazyData("wife.json", true)
@@ -43,22 +56,28 @@ func init() {
 		Handle(func(ctx *zero.Ctx) {
 			card := cards[fcext.RandSenderPerDayN(ctx.Event.UserID, len(cards))]
 			data, err := engine.GetLazyData("wives/"+card, true)
-			card, _, _ = strings.Cut(card, ".")
+			var msgText string
+			work, name := card2name(card)
+			if work != "" && name != "" {
+				msgText = fmt.Sprintf("今天的二次元老婆是~来自【%s】的【%s】哒", work, name)
+			} else {
+				msgText = fmt.Sprintf("今天的二次元老婆是~【%s】哒", card)
+			}
 			if err != nil {
 				ctx.SendChain(
 					message.At(ctx.Event.UserID),
-					message.Text("今天的二次元老婆是~【", card, "】哒\n【图片下载失败: ", err, "】"),
+					message.Text(msgText, "\n【图片下载失败: ", err, "】"),
 				)
 				return
 			}
 			if id := ctx.SendChain(
 				message.At(ctx.Event.UserID),
-				message.Text("今天的二次元老婆是~【", card, "】哒"),
+				message.Text(msgText),
 				message.ImageBytes(data),
 			); id.ID() == 0 {
 				ctx.SendChain(
 					message.At(ctx.Event.UserID),
-					message.Text("今天的二次元老婆是~【", card, "】哒\n【图片发送失败, 请联系维护者】"),
+					message.Text(msgText, "\n【图片发送失败, 多半是被夹了，请联系维护者】"),
 				)
 			}
 		})
