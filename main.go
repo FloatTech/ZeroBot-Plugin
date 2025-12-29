a193516fec6e36325bbd0614b6bd08c34b995430
小锅饭 <1156544355@qq.com>
2025-12-29 11:38:53 +0800

    feat: 添加小红书文案

--- main.go
+++ main.go
@@ -154,6 +154,7 @@ import (
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/wife"              // 抽老婆
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/wordcount"         // 聊天热词
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/wordle"            // 猜单词
+	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/xhstext"           // 小红书文案
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/ygocdb"            // 游戏王白鸽API卡查
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/ygotrade"          // 游戏王集换社卡价查询
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/ymgal"             // 月幕galgame
