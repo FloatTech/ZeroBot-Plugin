981f4c5dd70c8a37c5b5fe283f9056741ce6b817
源文雨 <41315874+fumiama@users.noreply.github.com>
2026-01-08 23:10:33 +0800

    🔖 v1.10.10

--- kanban/banner/banner.go
+++ kanban/banner/banner.go
@@ -3,13 +3,13 @@
 package banner
 
 // Version ...
-var Version = "v1.10.9"
+var Version = "v1.10.10"
 
 // Copyright ...
 var Copyright = "© 2020 - 2026 FloatTech"
 
 // Banner ...
 var Banner = "* OneBot + ZeroBot + Golang\n" +
-	"* Version " + Version + " - 2026-01-06 21:24:56 +0800 CST\n" +
+	"* Version " + Version + " - 2026-01-08 23:10:16 +0800 CST\n" +
 	"* Copyright " + Copyright + ". All Rights Reserved.\n" +
 	"* Project: https://github.com/FloatTech/ZeroBot-Plugin"
