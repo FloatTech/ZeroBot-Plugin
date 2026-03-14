0686d8389203e8bcedd5fe9e9cb0657468c610fe
源文雨 <41315874+fumiama@users.noreply.github.com>
2026-03-14 12:40:27 +0800

    feat: new rendercard & fix(aichat): agent nick

--- plugin/aichat/main.go
+++ plugin/aichat/main.go
@@ -114,8 +114,6 @@ func init() {
 			ctx.NoTimeout()
 			logrus.Debugln("[aichat] agent set no timeout")
 			hasresp := false
-			// ispuremsg := false
-			// hassavemem := false
 			for i := 0; i < 8; i++ { // 最大运行 8 轮因为问答上下文只有 16
 				reqs := chat.CallAgent(ag, zero.SuperUserPermission(ctx), i+1, x, mod, gid, role)
 				if len(reqs) == 0 {
@@ -126,22 +124,8 @@ func init() {
 				mp.Store(chat.StateKeyAgentTriggered, struct{}{})
 				for _, req := range reqs {
 					if req.Action == goba.SVM { // is a fake action
-						/*if hassavemem {
-							ag.AddTerminus(gid)
-							logrus.Warnln("[aichat] agent call save mem multi times, force inserting EOA")
-							return
-						}
-						hassavemem = true*/
 						continue
 					}
-					/*if req.Action == "send_private_msg" || req.Action == "send_group_msg" {
-						if ispuremsg {
-							ag.AddTerminus(gid)
-							logrus.Warnln("[aichat] agent call send msg multi times, force inserting EOA")
-							return
-						}
-						ispuremsg = true
-					}*/
 					logrus.Debugln("[chat] agent triggered", gid, "add requ:", &req)
 					ag.AddRequest(gid, &req)
 					rsp := ctx.CallAction(req.Action, req.Params)
